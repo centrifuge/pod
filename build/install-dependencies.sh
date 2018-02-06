dc21bc143689bb73b0f9996658735d9f8b248094
Philip Stehlik <p@pstehlik.com>
2018-02-06 13:23:29 -0500

    remove go-ethereum cache - can't get it to work

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -7,8 +7,6 @@ DEPBIN_CACH_FOLDER=$GOPATH/pkg/depbin
 DEPBIN_CACHED=$DEPBIN_CACH_FOLDER/dep
 DEPBIN=$GOPATH/bin/dep
 
-GO_ETH_CACHED=$GOPATH/src/github.com/ethereum/go-ethereum/VERSION
-
 if [ -e "${DEPBIN_CACHED}" ]
 then
     echo "Found existing dep binary in ${DEPBIN}. Not downloading again - just linking."
@@ -22,12 +20,3 @@ else
     chmod +x $DEPBIN_CACHED
     ln -s $DEPBIN_CACHED $DEPBIN    
 fi
-
-if [ -e "${GO_ETH_CACHED}" ]
-then
-    echo "Found ${GO_ETH_CACHED}. Not fetching go-ethereum again."
-else
-    echo "Fetching go-ethereum dependency as ${GO_ETH_CACHED} not found"
-    go get github.com/ethereum/go-ethereum
-fi
-
