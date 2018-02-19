ae105ac26209d9433fdd50c33949d94ed5635eba
Philip Stehlik <p@pstehlik.com>
2018-02-19 16:34:25 +0100

    adding go172 launch script as workaround for https://github.com/trufflesuite/truffle/issues/721

--- scripts/test-ethereum/prepare.sh
+++ scripts/test-ethereum/prepare.sh
@@ -3,5 +3,12 @@
 source ./env_vars.sh
 
 mkdir -p $DATA_DIR
+mkdir -p $DATA_DIR/keystore
 
 geth --identity "${IDENTITY}" --nodiscover --networkid=$NETWORK_ID --datadir=$DATA_DIR  init genesis.json
+
+#password := `ZhXfpAc#vHu4JTELA`
+cp coinbase.json $DATA_DIR/keystore/
+
+#password := `fenrwf34nr3cdlsmk`
+cp userAccount.json $DATA_DIR/keystore/
