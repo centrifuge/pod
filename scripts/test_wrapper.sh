78f1f102c2cbed1634b9d3676803d5e11fe51766
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-03-08 20:04:39 -0500

    WIP: Integration Testing

--- scripts/test_wrapper.sh
+++ scripts/test_wrapper.sh
@@ -24,7 +24,7 @@ npm install
 # Unlock User to Run Migration and Run it
 geth attach "http://localhost:${RPC_PORT}" --exec "personal.unlockAccount('0x${CENT_ETHEREUM_ACCOUNTS_MAIN_ADDRESS}', '${CENT_ETHEREUM_ACCOUNTS_MAIN_PASSWORD}')" && truffle migrate --network localgeth -f 2
 status=$?
-geth attach "http://localhost:${RPC_PORT}" --exec "personal"
+
 export CENT_ANCHOR_ETHEREUM_ANCHORREGISTRYADDRESS=`cat build/contracts/AnchorRegistry.json | jq -r --arg NETWORK_ID "${NETWORK_ID}" '.networks[$NETWORK_ID].address' | tr -d '\n'`
 cd ${PARENT_DIR}
 
