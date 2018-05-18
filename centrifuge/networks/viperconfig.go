fba07f3c7e48c7d0ae11a96f384e652ab237a3b1
Lucas Vogelsang <l@lucasvo.com>
2018-05-18 21:16:45 +0000

    Checkpoint: tests for viper network config

--- centrifuge/networks/viperconfig.go
+++ centrifuge/networks/viperconfig.go
@@ -1,12 +1,15 @@
 package networks
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/spf13/viper"
 )
 
 const (
 	NETWORK_DEFAULT_CONFIG_NAME = "networks"
-	NETWORK_DEFAULT_CONFIG_PATH = "resources"
+	NETWORK_DEFAULT_CONFIG_PATH = "../../resources"
+	BOOTSTRAP_PEERS_KEY         = "bootstrapPeers"
 )
 
 type ViperNetworkConfiguration struct {
@@ -18,12 +21,13 @@ func (cc *ViperNetworkConfiguration) GetNetworkString() string {
 	return cc.networkString
 }
 
-func (cc *ViperNetworkConfiguration) GetContractAddress(string) []byte {
-	return []byte{}
+func (cc *ViperNetworkConfiguration) GetContractAddress(contract string) (address []byte, err error) {
+	address, err = hex.DecodeString(cc.viperConfig.GetString(fmt.Sprintf("contractAddresses.%s", contract))[2:])
+	return
 }
 
-func (cc *ViperNetworkConfiguration) GetBootstrapPeers(string) []string {
-	return []string{""}
+func (cc *ViperNetworkConfiguration) GetBootstrapPeers() []string {
+	return cc.viperConfig.GetStringSlice(BOOTSTRAP_PEERS_KEY)
 }
 
 type ViperNetworkConfigurationLoader struct {
@@ -42,8 +46,8 @@ func (cl *ViperNetworkConfigurationLoader) SetNetworkConfigPath(path string) {
 
 func (cl *ViperNetworkConfigurationLoader) LoadNetworkConfig() (err error) {
 	c := viper.New()
-	c.SetConfigName("networks")
-	c.AddConfigPath("./resources")
+	c.SetConfigName(cl.networkConfigName)
+	c.AddConfigPath(cl.networkConfigPath)
 	err = c.ReadInConfig()
 	if err != nil {
 		return err
@@ -54,7 +58,7 @@ func (cl *ViperNetworkConfigurationLoader) LoadNetworkConfig() (err error) {
 
 func (cl *ViperNetworkConfigurationLoader) GetConfigurationFromKey(key string) (vc *ViperNetworkConfiguration, err error) {
 	vc = &ViperNetworkConfiguration{networkString: key}
-	vc.viperConfig = cl.networksConfig.Sub(key)
+	vc.viperConfig = cl.networksConfig.Sub(fmt.Sprintf("networks.%s", key))
 	return
 }
 
--- centrifuge/networks/viperconfig_test.go
+++ centrifuge/networks/viperconfig_test.go
@@ -3,6 +3,7 @@
 package networks
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -12,7 +13,33 @@ func TestViperNetworkConfigurationLoader_LoadNetworkConfig(t *testing.T) {
 	cl := NewViperNetworkConfigurationLoader()
 	err := cl.LoadNetworkConfig()
 	assert.Nil(t, err)
-	fmt.Println(cl.networksConfig)
+	// Check a known value from the default configuration
+	assert.Equal(t, 4, cl.networksConfig.GetInt("networks.centrifuge-russianhill-eth-rinkeby.ethereumNetworkId"))
 
-	assert.Equal(t, 4, cl.networksConfig.GetInt("centrifuge-russianhill-eth-rinkeby.ethereumNetworkId"))
+	networkString := "centrifuge-russianhill-eth-rinkeby"
+	conf, err := cl.GetConfigurationFromKey(networkString)
+	assert.Nil(t, err)
+	assert.Equal(t, networkString, conf.GetNetworkString())
+
+	contractId, err := conf.GetContractAddress("identityFactory")
+	fmt.Println(err)
+	expectedContractId, _ := hex.DecodeString("0589ed482af8d6809f022fc11aa399fc8a883d52")
+	assert.Equal(t, expectedContractId, contractId)
+
+	expectedBootstrapPeers := []string{
+		"/ip4/127.0.0.1/tcp/38202/ipfs/QmTQxbwkuZYYDfuzTbxEAReTNCLozyy558vQngVvPMjLYk",
+		"/ip4/127.0.0.1/tcp/38203/ipfs/QmVf6EN6mkqWejWKW2qPu16XpdG3kJo1T3mhahPB5Se5n1",
+	}
+	bootstrapPeers := conf.GetBootstrapPeers()
+	assert.Equal(t, expectedBootstrapPeers, bootstrapPeers)
+
+	// Try to load a nonexistent configuration
+	cl = &ViperNetworkConfigurationLoader{
+		networkConfigPath: ".",
+		networkConfigName: "doesnotexist",
+	}
+	err = cl.LoadNetworkConfig()
+	assert.Error(t, err)
+	fmt.Println(err)
+
 }
