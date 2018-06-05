b66ca08a2ffa5fd2e806905fa386373eae7412e4
Lucas Vogelsang <l@lucasvo.com>
2018-06-05 10:22:34 -0400

    WiP Testing

--- scripts/tests/run_integration_tests.sh
+++ scripts/tests/run_integration_tests.sh
@@ -9,10 +9,10 @@ source "${PARENT_DIR}/scripts/test-dependencies/test-ethereum/env_vars.sh"
 export CENT_NETWORKS_TESTING_CONTRACTADDRESSES_ANCHORREGISTRY=`cat $CENT_ETHEREUM_CONTRACTS_DIR/build/contracts/AnchorRegistry.json | jq -r --arg NETWORK_ID "${NETWORK_ID}" '.networks[$NETWORK_ID].address' | tr -d '\n'`
 export CENT_NETWORKS_TESTING_CONTRACTADDRESSES_IDENTITYFACTORY=`cat $CENT_ETHEREUM_CONTRACTS_DIR/build/contracts/IdentityFactory.json | jq -r --arg NETWORK_ID "${NETWORK_ID}" '.networks[$NETWORK_ID].address' | tr -d '\n'`
 export CENT_NETWORKS_TESTING_CONTRACTADDRESSES_IDENTITYREGISTRY=`cat $CENT_ETHEREUM_CONTRACTS_DIR/build/contracts/IdentityRegistry.json | jq -r --arg NETWORK_ID "${NETWORK_ID}" '.networks[$NETWORK_ID].address' | tr -d '\n'`
-export CENT_NETWORKS_TESTING_CONTRACTADDRESSES_IDENTITY=`cat $CENT_ETHEREUM_CONTRACTS_DIR/build/contracts/Identity.json | jq -r --arg NETWORK_ID "${NETWORK_ID}" '.networks[$NETWORK_ID].address' | tr -d '\n'`
 echo "ANCHOR ADDRESS: ${CENT_NETWORKS_TESTING_CONTRACTADDRESSES_ANCHORREGISTRY}"
 echo "IDENTITY FACTORY ADDRESS: ${CENT_NETWORKS_TESTING_CONTRACTADDRESSES_IDENTITYFACTORY}"
 echo "IDENTITY REGISTRY ADDRESS: ${CENT_NETWORKS_TESTING_CONTRACTADDRESSES_IDENTITYREGISTRY}"
+
 ############################################################
 
 echo "Running Integration Ethereum Tests against [${CENT_ETHEREUM_NODEURL}]"
