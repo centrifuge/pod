f04e0709d4fd97fa848ff4c9f44194581abaa85b
Lucas Vogelsang <l@lucasvo.com>
2018-06-26 03:49:45 +0000

    Adding timestamp output

--- scripts/tests/run_integration_tests.sh
+++ scripts/tests/run_integration_tests.sh
@@ -35,7 +35,7 @@ echo "IDENTITY FACTORY ADDRESS: ${vtemp2}"
 #
 echo "Running Integration Ethereum Tests against [${CENT_ETHEREUM_NODEURL}] with TIMEOUT [${TEST_TIMEOUT}]"
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic -tags=ethereum -timeout ${TEST_TIMEOUT} $d
+    go test -v -coverprofile=profile.out -covermode=atomic -tags=ethereum -timeout ${TEST_TIMEOUT} $d | ts '[%Y-%m-%d %H:%M:%S]'
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
--- scripts/tests/run_unit_tests.sh
+++ scripts/tests/run_unit_tests.sh
@@ -3,7 +3,7 @@
 echo "Running Unit Tests"
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic -tags=unit $d
+    go test -v -coverprofile=profile.out -covermode=atomic -tags=unit $d | ts '[%Y-%m-%d %H:%M:%S]'
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
