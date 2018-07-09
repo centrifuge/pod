8f59b619d21252bdb2794d1a83dc4f70e0ac360a
Lucas Vogelsang <l@lucasvo.com>
2018-07-09 21:32:54 +0000

    check for pemfile

--- centrifuge/keytools/keytools.go
+++ centrifuge/keytools/keytools.go
@@ -30,6 +30,9 @@ func readKeyFromPemFile(fileName, keyType string) (key []byte, err error) {
 		return []byte{}, err
 	}
 	block, _ := pem.Decode(pemData)
+	if block == nil {
+		return []byte{}, fmt.Errorf("File [%s] is not a valid pem file", fileName)
+	}
 	if block.Type != keyType {
 		return []byte{}, fmt.Errorf("Key type mismatch got [%s] but expected [%s]", block.Type, keyType)
 	}
