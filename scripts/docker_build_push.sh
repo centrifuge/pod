83e8448b5b1589067a7adf576eee3d0bfcfc84fe
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-07-16 17:37:10 +0200

    WIP - Adding Binary generation to CI

--- scripts/docker_build_push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git_sha="$(git rev-parse --short HEAD)"
-docker build -t ${IMAGE_NAME}:${git_sha} .
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag "${IMAGE_NAME}:${git_sha}" "${IMAGE_NAME}:latest"
-docker push ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}:${git_sha}
