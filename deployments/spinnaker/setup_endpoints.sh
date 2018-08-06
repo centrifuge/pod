d70a704f1d146f72a45f4ebd18d8d9b602148f5a
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-08-06 15:01:29 +0200

    removed spinnaker

--- deployments/spinnaker/setup_endpoints.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl patch svc spin-gate -p '{"spec":{"type":"LoadBalancer"}}' -n spinnaker
-kubectl patch svc spin-deck -p '{"spec":{"type":"LoadBalancer"}}' -n spinnaker
-echo "Wait for until endpoints populated"
