74f0e95d3d9ceb94c950ad591498cca81e14ce87
Vimukthi Wickramasinghe <vimukthi@centrifuge.io>
2018-08-12 15:13:56 +0200

    First round

--- centrifuge/queue/queue.go
+++ centrifuge/queue/queue.go
@@ -17,16 +17,16 @@ type Queue interface {
 	Start()
 
 	// msg can be any deserialized struct, should we change the type to bytes?
-	Enqueue(queueName string, msg string, options *EnqueueOptions)
+	Enqueue(queueName string, msg string, options *EnqueueOptions) error
 
 	// Dequeue the message but resurface it after the set timeOut
 	// (Pull model)
-	Dequeue(queue string) (id, msg string, options *EnqueueOptions)
+	Dequeue(queue string) (id, msg string, options *EnqueueOptions, err error)
 
 	// Delete the message with the given id, no resurface afterwards
-	Delete(queue, id string)
+	Delete(queue, id string) error
 
-	DeleteAll(queue string)
+	DeleteAll(queue string) error
 
 	Stop()
 }
@@ -41,7 +41,7 @@ const (
 
 // A handler function receives a single message from a queue and handles it(after deserializing to proper type), returning a proper status after the execution
 // Rationale: abstract away the queuing details from business logic. Makes it easier to test the handlers.
-type Handler func(msg string, options *EnqueueOptions) HandlerStatus
+type Handler func(msg string, options *EnqueueOptions) (HandlerStatus, error)
 
 // Worker interface is an abstraction over all queue message receivers (go routines).
 // It might contain queuing system specific details such as retry logic based on EnqueueOptions.
