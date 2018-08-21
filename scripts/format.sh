1a727620110404b78e968d2510e469dd6cd747c4
Vimukthi Wickramasinghe <vimukthi@centrifuge.io>
2018-08-21 10:41:53 +0200

    Fix script path

--- scripts/format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-if [ -n "$(goimports -d -e -l .)" ]; then
-    echo "Go code is not formatted:"
-    goimports -d -e -l .
-    exit 1
-fi
---
+++ scripts/format_go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ -n "$($GOPATH/bin/goimports -d -e -l .)" ]; then
+    echo "Go code is not formatted:"
+    $GOPATH/bin/goimports -d -e -l .
+    exit 1
+fi
