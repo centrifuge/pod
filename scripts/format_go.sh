c11f6b20d0569e52a58e23f23c7118edaeabe6c7
Vimukthi Wickramasinghe <vimukthi@centrifuge.io>
2018-08-21 10:51:31 +0200

    Fix more

--- scripts/format_go.sh
+++ scripts/format_go.sh
@@ -4,6 +4,6 @@ set -x
 
 if [ -n "$($GOPATH/bin/goimports -d -e -l .)" ]; then
     echo "Go code is not formatted:"
-    $GOPATH/bin/goimports -d -e -l .
+    $GOPATH/bin/goimports -d -e -l ./centrifuge
     exit 1
 fi
