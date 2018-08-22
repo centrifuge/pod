851a3b1bdf565fa4580bcb5f614984ca1ee5ac6d
vedhavyas <vedhavyas.singareddi@gmail.com>
2018-08-22 11:46:05 +0530

    more tests

--- centrifuge/storage/leveldb_test.go
+++ centrifuge/storage/leveldb_test.go
@@ -3,33 +3,108 @@
 package storage
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/CentrifugeInc/centrifuge-protobufs/gen/go/coredocument"
+	"github.com/CentrifugeInc/centrifuge-protobufs/gen/go/purchaseorder"
+	"github.com/CentrifugeInc/go-centrifuge/centrifuge/tools"
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var dbFileName = "/tmp/centrifuge_testing_storage.leveldb"
 var storageDb *leveldb.DB
+var defaultDB *DefaultLevelDB
 
 func TestMain(m *testing.M) {
 	storageDb = NewLevelDBStorage(dbFileName)
 	defer storageDb.Close()
+	defaultDB = &DefaultLevelDB{LevelDB: storageDb}
 
 	result := m.Run()
 	os.RemoveAll(dbFileName)
 	os.Exit(result)
 }
 
-func TestGetLeveldbStorage(t *testing.T) {
+func TestGetLevelDBStorage(t *testing.T) {
 	one := []byte("1")
 	two := []byte("2")
 
 	err := storageDb.Put(one, two, nil)
 	assert.Nil(t, err, "Should not error out")
 
-	get_one, err := storageDb.Get(one, nil)
+	getOnes, err := storageDb.Get(one, nil)
 	assert.Nil(t, err, "Should not error out")
-	assert.Equal(t, two, get_one)
+	assert.Equal(t, two, getOnes)
+}
+
+func TestDefaultLevelDB_Create(t *testing.T) {
+	id := tools.RandomSlice32()
+	order := purchaseorderpb.PurchaseOrderDocument{CoreDocument: &coredocumentpb.CoreDocument{DocumentIdentifier: id}}
+	err := defaultDB.Create(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Nil(t, err, "create must pass")
+
+	err = defaultDB.Create(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Error(t, err, "create must fail")
+
+	defaultDB.ValidateFunc = func([]byte, proto.Message) error {
+		return fmt.Errorf("failed validation")
+	}
+
+	id2 := tools.RandomSlice32()
+	order.CoreDocument.DocumentIdentifier = id2
+	err = defaultDB.Create(id2, &order)
+	assert.Error(t, err, "create must fail")
+	defaultDB.ValidateFunc = nil
+}
+
+func TestDefaultLevelDB_Exists(t *testing.T) {
+	id1 := tools.RandomSlice32()
+	id2 := tools.RandomSlice32()
+	order := purchaseorderpb.PurchaseOrderDocument{CoreDocument: &coredocumentpb.CoreDocument{DocumentIdentifier: id1}}
+	err := defaultDB.Create(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Nil(t, err, "create must pass")
+
+	assert.True(t, defaultDB.Exists(id1))
+	assert.False(t, defaultDB.Exists(id2))
+}
+
+func TestDefaultLevelDB_GetKey(t *testing.T) {
+	id := tools.RandomSlice32()
+	assert.Equal(t, id, defaultDB.GetKey(id))
+}
+
+func TestDefaultLevelDB_GetByID(t *testing.T) {
+	id := tools.RandomSlice32()
+	order := purchaseorderpb.PurchaseOrderDocument{CoreDocument: &coredocumentpb.CoreDocument{DocumentIdentifier: id}}
+	err := defaultDB.Create(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Nil(t, err, "create must pass")
+	order2 := new(purchaseorderpb.PurchaseOrderDocument)
+	assert.Nil(t, defaultDB.GetByID(id, order2), "error should be nil")
+	assert.Equal(t, id, order2.CoreDocument.DocumentIdentifier)
+}
+
+func TestDefaultLevelDB_Update(t *testing.T) {
+	id := tools.RandomSlice32()
+	order := purchaseorderpb.PurchaseOrderDocument{CoreDocument: &coredocumentpb.CoreDocument{DocumentIdentifier: id}}
+	err := defaultDB.Update(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Error(t, err, "create must fail")
+
+	err = defaultDB.Create(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Nil(t, err, "create must pass")
+
+	err = defaultDB.Update(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Nil(t, err, "update must pass")
+
+	defaultDB.ValidateFunc = func([]byte, proto.Message) error {
+		return fmt.Errorf("failed validation")
+	}
+
+	err = defaultDB.Update(order.CoreDocument.DocumentIdentifier, &order)
+	assert.Error(t, err, "update must fail")
+
+	defaultDB.ValidateFunc = nil
 }
