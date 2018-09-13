3f1fbcf802dc51e390c0be0fbed584b441eeb505
Manuel Polzhofer <manuel@centrifuge.io>
2018-09-13 11:40:53 +0200

    added optional rich-go to every test and to travis

--- scripts/tests/run_unit_tests.sh
+++ scripts/tests/run_unit_tests.sh
@@ -3,12 +3,19 @@
 echo "Running Unit Tests"
 
 status=$?
+
+
 for d in $(go list ./... | grep -v vendor); do
-    output=$(go test -v -race -coverprofile=profile.out -covermode=atomic -tags=unit $d 2>&1 | tee >(richgo testfilter))
+    output="go test -v -race -coverprofile=profile.out -covermode=atomic -tags=unit $d 2>&1"
+
+    if [ -x "$(command -v richgo)" ]; then
+     output="$output | tee >(richgo testfilter)"
+    fi
+
     if [ $? -ne 0 ]; then
       status=1
     fi
-    echo "${output}" | while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done
+    eval "$output" | while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
