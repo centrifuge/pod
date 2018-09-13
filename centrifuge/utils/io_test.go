70aea78d461f8c0d796dd21a91d9c3b9dc4b2ca3
Manuel Polzhofer <manuel@centrifuge.io>
2018-09-13 14:27:38 +0200

    added tests for io package

---
+++ centrifuge/utils/io_test.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"os"
+	"testing"
+
+	"github.com/CentrifugeInc/go-centrifuge/centrifuge/tools"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	PublicKey  = "PUBLIC KEY"
+	PrivateKey = "PRIVATE KEY"
+)
+
+func TestWriteAndReadPemFile(t *testing.T) {
+
+	randomBytes := tools.RandomSlice(32)
+	testFileName := "test.file"
+	err := WriteKeyToPemFile(testFileName, PrivateKey, randomBytes)
+
+	assert.Nil(t, err, "writing file failed")
+
+	fileContent, err := ReadKeyFromPemFile(testFileName, PrivateKey)
+
+	assert.Nil(t, err, "failed to read written file correctly")
+
+	assert.Equal(t, randomBytes, fileContent, "content of the written file is not correct")
+
+	fileContent, err = ReadKeyFromPemFile(testFileName, PublicKey)
+
+	assert.Error(t, err, "should produce an error because key file contains a private key ")
+
+	os.Remove(testFileName)
+
+}
