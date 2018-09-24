67c212c497bfc26a7d01e77e6020008e55f421a6
vedhavyas <vedhavyas.singareddi@gmail.com>
2018-09-24 13:44:22 +0200

    use https for api

--- scripts/push_to_swagger.sh
+++ scripts/push_to_swagger.sh
@@ -4,7 +4,7 @@
 echo "pushing swagger.json to SwaggerHub"
 
 curl -i -X POST \
-  http://api.swaggerhub.com/apis/centrifuge.io/cent-node?version=0.0.1 \
+  https://api.swaggerhub.com/apis/centrifuge.io/cent-node?version=0.0.1 \
   -H "Authorization: $SWAGGER_API_KEY" \
   -H "Content-Type: application/json" -d @./centrifuge/protobufs/gen/swagger.json
 
