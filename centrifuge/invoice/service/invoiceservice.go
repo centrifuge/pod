dac9cfbd4a4d06516ba91aac70621b3729ddbbcd
Lucas Vogelsang <l@lucasvo.com>
2018-09-26 18:32:59 +0000

    Goimport

--- centrifuge/invoice/service/invoiceservice.go
+++ centrifuge/invoice/service/invoiceservice.go
@@ -2,9 +2,6 @@ package invoiceservice
 
 import (
 	"fmt"
-
-	"github.com/centrifuge/go-centrifuge/centrifuge/invoice"
-
 	"github.com/centrifuge/centrifuge-protobufs/gen/go/coredocument"
 	"github.com/centrifuge/centrifuge-protobufs/gen/go/invoice"
 	"github.com/centrifuge/go-centrifuge/centrifuge/centerrors"
