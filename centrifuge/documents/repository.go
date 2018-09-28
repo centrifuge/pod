a5e57fcde455b47bfc6ccf955e6ff5e40c25334a
vedhavyas <vedhavyas.singareddi@gmail.com>
2018-09-28 14:14:11 +0200

    move repository under docuemnts

---
+++ centrifuge/documents/repository.go
@@ -0,0 +1,123 @@
+package documents
+
+import (
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+// Getter interface can be implemented by any type that handles document retrieval
+type Getter interface {
+	// GetKey will prepare the the identifier key from ID
+	GetKey(id []byte) (key []byte)
+
+	// GetByID finds the doc with identifier and marshals it into message
+	GetModelByID(id []byte, model Model) error
+}
+
+// Checker interface can be implemented by any type that handles if document exists
+type Checker interface {
+	// Exists checks for document existence
+	// True if exists else false
+	Exists(id []byte) bool
+}
+
+// Creator interface can be implemented by any type that handles document creation
+type Creator interface {
+	// Create stores the initial document
+	// If document exist, it errors out
+	Create(id []byte, model Model) error
+}
+
+// Updater interface can be implemented by any type that handles document update
+type Updater interface {
+	// Update updates the already stored document
+	// errors out when document is missing
+	Update(id []byte, model Model) error
+}
+
+// Repository should be implemented by any type that wants to store a document in key-value storage
+type Repository interface {
+	Checker
+	Getter
+	Creator
+	Updater
+}
+
+// LevelDBRepository is implements repository
+type LevelDBRepository struct {
+	KeyPrefix string
+	LevelDB   *leveldb.DB
+}
+
+// Exists returns if the document exists in the repository
+func (repo *LevelDBRepository) Exists(id []byte) bool {
+	_, err := repo.LevelDB.Get(repo.GetKey(id), nil)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// GetKey prepends the id with prefix and returns the result
+func (repo *LevelDBRepository) GetKey(id []byte) []byte {
+	return append([]byte(repo.KeyPrefix), id...)
+}
+
+// LoadByID finds the document by id and marshals into message
+func (repo *LevelDBRepository) LoadByID(id []byte, model Model) error {
+	if model == nil {
+		return fmt.Errorf("nil document provided")
+	}
+
+	data, err := repo.LevelDB.Get(repo.GetKey(id), nil)
+	if err != nil {
+		return err
+	}
+
+	err = model.FromJSON(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Create creates the document if not exists
+// errors out if document exists
+func (repo *LevelDBRepository) Create(id []byte, model Model) error {
+	if model == nil {
+		return fmt.Errorf("nil model provided")
+	}
+
+	if repo.Exists(id) {
+		return fmt.Errorf("document already exists")
+	}
+
+	data, err := model.JSON()
+	if err != nil {
+		return err
+	}
+
+	return repo.LevelDB.Put(repo.GetKey(id), data, nil)
+}
+
+// Update updates the doc with ID if exists
+// errors out if the document
+func (repo *LevelDBRepository) Update(id []byte, model Model) error {
+	if model == nil {
+		return fmt.Errorf("nil document provided")
+	}
+
+	if !repo.Exists(id) {
+		return fmt.Errorf("document doesn't exists")
+	}
+
+	data, err := model.JSON()
+	if err != nil {
+		return err
+	}
+
+	return repo.LevelDB.Put(repo.GetKey(id), data, nil)
+}
---
+++ centrifuge/documents/repository_test.go
@@ -0,0 +1,119 @@
+// +build unit
+
+package documents
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/centrifuge/centrifuge-protobufs/gen/go/coredocument"
+	cc "github.com/centrifuge/go-centrifuge/centrifuge/context/testingbootstrap"
+	"github.com/centrifuge/go-centrifuge/centrifuge/tools"
+	"github.com/stretchr/testify/assert"
+)
+
+var levelDB LevelDBRepository
+
+func TestMain(m *testing.M) {
+	cc.TestIntegrationBootstrap()
+	levelDB = LevelDBRepository{LevelDB: cc.GetLevelDBStorage()}
+	flag.Parse()
+	result := m.Run()
+	cc.TestIntegrationTearDown()
+	os.Exit(result)
+}
+
+type model struct {
+	shouldError bool
+	Data        string `json:"data"`
+}
+
+func (m *model) Type() reflect.Type                                     { return reflect.TypeOf(m) }
+func (m *model) CoreDocument() (*coredocumentpb.CoreDocument, error)    { return nil, nil }
+func (m *model) FromCoreDocument(cd *coredocumentpb.CoreDocument) error { return nil }
+
+func (m *model) JSON() ([]byte, error) {
+	if m.shouldError {
+		return nil, fmt.Errorf("failed to marshal")
+	}
+
+	return json.Marshal(m)
+}
+
+func (m *model) FromJSON(data []byte) error {
+	if m.shouldError {
+		return fmt.Errorf("failed to unmarshal")
+	}
+
+	return json.Unmarshal(data, m)
+}
+
+func TestDefaultLevelDB_LoadByID(t *testing.T) {
+	id := tools.RandomSlice(32)
+
+	// missing ID
+	err := levelDB.LoadByID(id, new(model))
+	assert.Error(t, err, "error must be non nil")
+
+	// nil document
+	err = levelDB.LoadByID(id, nil)
+	assert.Error(t, err, "error must be non nil")
+
+	// Failed unmarshal
+	m := &model{shouldError: true}
+	err = levelDB.LoadByID(id, m)
+	assert.Error(t, err, "error must be non nil")
+
+	// success
+	m = &model{Data: "hello, world"}
+	err = levelDB.Create(id, m)
+	assert.Nil(t, err, "error should be nil")
+	nm := new(model)
+	err = levelDB.LoadByID(id, nm)
+	assert.Nil(t, err, "error should be nil")
+	assert.Equal(t, m, nm, "models must match")
+}
+
+func TestDefaultLevelDB_Create(t *testing.T) {
+	id := tools.RandomSlice(32)
+	d := &model{Data: "Create it"}
+	err := levelDB.Create(id, d)
+	assert.Nil(t, err, "create must pass")
+
+	// same id
+	err = levelDB.Create(id, new(model))
+	assert.Error(t, err, "create must fail")
+
+	// nil model
+	err = levelDB.Create(id, nil)
+	assert.Error(t, err, "create must fail")
+}
+
+func TestDefaultLevelDB_UpdateModel(t *testing.T) {
+	id := tools.RandomSlice(32)
+
+	// missing Id
+	err := levelDB.Update(id, new(model))
+	assert.Error(t, err, "update must fail")
+
+	// nil model
+	err = levelDB.Update(id, nil)
+	assert.Error(t, err, "update must fail")
+
+	m := &model{Data: "create it"}
+	err = levelDB.Create(id, m)
+	assert.Nil(t, err, "create must pass")
+
+	// successful one
+	m.Data = "update it"
+	err = levelDB.Update(id, m)
+	assert.Nil(t, err, "update must pass")
+	nm := new(model)
+	err = levelDB.LoadByID(id, nm)
+	assert.Nil(t, err, "get mode must pass")
+	assert.Equal(t, m, nm)
+}
