bab49e9e93576e7fd6b3229a8dca72fd160492c7
Lucas Vogelsang <l@lucasvo.com>
2018-10-02 21:59:20 +0000

    fixing url

--- centrifuge/protobufs/gen/go/nft/service.pb.gw.go
+++ centrifuge/protobufs/gen/go/nft/service.pb.gw.go
@@ -116,7 +116,7 @@ func RegisterNFTServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 }
 
 var (
-	pattern_NFTService_MintNFT_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"tokens", "mint"}, ""))
+	pattern_NFTService_MintNFT_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"token", "mint"}, ""))
 )
 
 var (
