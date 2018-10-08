1e6a3a52cf20e8a75891f2e82ff13f58f2b8a995
Manuel Polzhofer <manuel@centrifuge.io>
2018-10-08 16:09:55 +0200

    added comments

--- centrifuge/documents/error.go
+++ centrifuge/documents/error.go
@@ -94,6 +94,7 @@ func addToMap(errorMap map[string]string, key, msg string) map[string]string {
 	return errorMap
 }
 
+// ConvertToMap converts errors into a map.
 func ConvertToMap(err error) map[string]string {
 
 	errorMap := make(map[string]string)
