aabc07647942addb975514ea558213dabf177dc3
Vimukthi <vbwickramasinghe@gmail.com>
2018-10-10 10:17:23 +0200

    Proof endpoint protobufs (#255)
    * Proof endpoint protobufs
    
    * minor
    
    * Fix
    
    * Review comments
    
    * Adding proof endpoint for particular version
    
    * Adding proof endpoint for particular version
    
    * Adding proof endpoint for particular version
    
    * Adding proof endpoint for particular version
    
    * Minor updates
    
    * Minor updates
    
    * Update generated code

--- centrifuge/api/service.go
+++ centrifuge/api/service.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"github.com/centrifuge/go-centrifuge/centrifuge/coredocument/processor"
+	"github.com/centrifuge/go-centrifuge/centrifuge/documents"
 	"github.com/centrifuge/go-centrifuge/centrifuge/documents/invoice"
 	"github.com/centrifuge/go-centrifuge/centrifuge/healthcheck/controller"
 	"github.com/centrifuge/go-centrifuge/centrifuge/identity"
 	"github.com/centrifuge/go-centrifuge/centrifuge/p2p"
+	"github.com/centrifuge/go-centrifuge/centrifuge/protobufs/gen/go/documents"
 	"github.com/centrifuge/go-centrifuge/centrifuge/protobufs/gen/go/health"
 	"github.com/centrifuge/go-centrifuge/centrifuge/protobufs/gen/go/invoice"
 	legacyInvoice "github.com/centrifuge/go-centrifuge/centrifuge/protobufs/gen/go/legacy/invoice"
@@ -18,25 +20,35 @@ import (
 
 // registerServices registers all endpoints to the grpc server
 func registerServices(ctx context.Context, grpcServer *grpc.Server, gwmux *runtime.ServeMux, addr string, dopts []grpc.DialOption) error {
-	invoicepb.RegisterDocumentServiceServer(grpcServer, invoice.GRPCHandler(invoice.DefaultService(
-		invoice.GetRepository(),
-		coredocumentprocessor.DefaultProcessor(identity.IDService, p2p.NewP2PClient()))))
-	err := invoicepb.RegisterDocumentServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
+	// documents (common)
+	documentpb.RegisterDocumentServiceServer(grpcServer, documents.GRPCHandler())
+	err := documentpb.RegisterDocumentServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
 	if err != nil {
 		return err
 	}
 
+	// invoice
+	invoicepb.RegisterDocumentServiceServer(grpcServer, invoice.GRPCHandler(invoice.DefaultService(
+		invoice.GetRepository(),
+		coredocumentprocessor.DefaultProcessor(identity.IDService, p2p.NewP2PClient()))))
+	err = invoicepb.RegisterDocumentServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
+	if err != nil {
+		return err
+	}
 	legacyInvoice.RegisterInvoiceDocumentServiceServer(grpcServer, invoice.LegacyGRPCHandler())
 	err = legacyInvoice.RegisterInvoiceDocumentServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
 	if err != nil {
 		return err
 	}
+
+	// purchase orders
 	purchaseorderpb.RegisterPurchaseOrderDocumentServiceServer(grpcServer, &purchaseordercontroller.PurchaseOrderDocumentController{})
 	err = purchaseorderpb.RegisterPurchaseOrderDocumentServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
 	if err != nil {
 		return err
 	}
 
+	// healthcheck
 	healthpb.RegisterHealthCheckServiceServer(grpcServer, &healthcheckcontroller.HealthCheckController{})
 	err = healthpb.RegisterHealthCheckServiceHandlerFromEndpoint(ctx, gwmux, addr, dopts)
 	if err != nil {
