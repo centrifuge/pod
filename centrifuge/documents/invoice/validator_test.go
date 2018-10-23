0ec4019f8b9092a3219bc186a104f6aa69940e7f
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2018-10-23 17:28:01 +0200

    Legacy/invoice (#357)
    Remove legacy invoice. Merge once functional tests have migrated to new APIs

--- centrifuge/documents/invoice/validator_test.go
+++ centrifuge/documents/invoice/validator_test.go
@@ -31,14 +31,14 @@ func TestFieldValidator_Validate(t *testing.T) {
 	assert.Contains(t, errs[0].Error(), "unknown document type")
 
 	// fail
-	err = fv.Validate(nil, new(InvoiceModel))
+	err = fv.Validate(nil, new(Invoice))
 	assert.Error(t, err)
 	errs = documents.Errors(err)
 	assert.Len(t, errs, 1, "errors length must be 2")
 	assert.Contains(t, errs[0].Error(), "currency is invalid")
 
 	// success
-	err = fv.Validate(nil, &InvoiceModel{
+	err = fv.Validate(nil, &Invoice{
 		Currency: "EUR",
 	})
 	assert.Nil(t, err)
@@ -81,7 +81,7 @@ func TestDataRootValidation_Validate(t *testing.T) {
 	// mismatch
 	contextHeader, err := documents.NewContextHeader()
 	assert.Nil(t, err)
-	inv := new(InvoiceModel)
+	inv := new(Invoice)
 	err = inv.InitInvoiceInput(testingdocuments.CreateInvoicePayload(), contextHeader)
 	assert.Nil(t, err)
 	inv.CoreDocument = cd
@@ -90,7 +90,7 @@ func TestDataRootValidation_Validate(t *testing.T) {
 	assert.Contains(t, err.Error(), "mismatched data root")
 
 	// success
-	inv = new(InvoiceModel)
+	inv = new(Invoice)
 	err = inv.InitInvoiceInput(testingdocuments.CreateInvoicePayload(), contextHeader)
 	assert.Nil(t, err)
 	err = inv.calculateDataRoot()
