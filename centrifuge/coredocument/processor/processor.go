6968767ab62b2d2a9d9c56ee69e63d80017a810b
Manuel Polzhofer <manuel@centrifuge.io>
2018-10-23 18:02:48 +0200

    removed legacy funcs

--- centrifuge/coredocument/processor/processor.go
+++ centrifuge/coredocument/processor/processor.go
@@ -10,7 +10,6 @@ import (
 	"github.com/centrifuge/go-centrifuge/centrifuge/centerrors"
 	"github.com/centrifuge/go-centrifuge/centrifuge/config"
 	"github.com/centrifuge/go-centrifuge/centrifuge/coredocument"
-	"github.com/centrifuge/go-centrifuge/centrifuge/coredocument/repository"
 	"github.com/centrifuge/go-centrifuge/centrifuge/documents"
 	"github.com/centrifuge/go-centrifuge/centrifuge/identity"
 	"github.com/centrifuge/go-centrifuge/centrifuge/keytools/ed25519keys"
@@ -29,8 +28,6 @@ var log = logging.Logger("coredocument")
 // E.g. send, anchor, etc.
 type Processor interface {
 	Send(ctx context.Context, coreDocument *coredocumentpb.CoreDocument, recipient identity.CentID) (err error)
-	Anchor(ctx context.Context, document *coredocumentpb.CoreDocument,
-		saveState func(coreDoc *coredocumentpb.CoreDocument) error) (err error)
 	PrepareForSignatureRequests(model documents.Model) error
 	RequestSignatures(ctx context.Context, model documents.Model) error
 	PrepareForAnchoring(model documents.Model) error
@@ -104,123 +101,6 @@ func (dp defaultProcessor) Send(ctx context.Context, coreDocument *coredocumentp
 		return err
 	}
 
-	return nil
-}
-
-// Anchor anchors the given CoreDocument
-// This method should:
-// - calculate the signing root
-// - sign document with own key
-// - collect signatures (incl. validate)
-// - store signatures on coredocument
-// - calculate DocumentRoot
-// - store doc in db
-// - anchor the document
-// - send anchored document to collaborators [NOT NEEDED since we do this in the current flow already because HandleSend****Document does it after anchoring]
-// Deprecated: use individual flows instead
-func (dp defaultProcessor) Anchor(
-	ctx context.Context,
-	document *coredocumentpb.CoreDocument,
-	saveState func(coreDoc *coredocumentpb.CoreDocument) error) error {
-
-	if document == nil {
-		return centerrors.NilError(document)
-	}
-
-	anchorID, err := anchors.NewAnchorID(document.CurrentVersion)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	// calculate the signing root
-	err = coredocument.CalculateSigningRoot(document)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	// sign document with own key and append it to signatures
-	idConfig, err := ed25519keys.GetIDConfig()
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-	sig := signatures.Sign(idConfig, document.SigningRoot)
-	document.Signatures = append(document.Signatures, sig)
-
-	if saveState != nil {
-		err = saveState(document)
-		if err != nil {
-			return centerrors.Wrap(err, "failed to save state")
-		}
-	}
-
-	// collect signatures (incl. validate)
-	// store signatures on coredocument
-	err = dp.P2PClient.GetSignaturesForDocument(ctx, document)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "failed to collect signatures")
-	}
-
-	if saveState != nil {
-		err = saveState(document)
-		if err != nil {
-			return centerrors.Wrap(err, "failed to save state")
-		}
-	}
-
-	// calculate DocumentRoot
-	err = coredocument.CalculateDocumentRoot(document)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	if saveState != nil {
-		err = saveState(document)
-		if err != nil {
-			return centerrors.Wrap(err, "failed to save state")
-		}
-	}
-
-	// store doc in db
-	err = coredocumentrepository.GetRepository().Create(document.CurrentVersion, document)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	rootHash, err := anchors.NewDocRoot(document.DocumentRoot)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	myCentID, err := identity.ToCentID(idConfig.ID)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	// generate message authentication code for the anchor call
-	secpIDConfig, err := secp256k1.GetIDConfig()
-	mac, err := secp256k1.SignEthereum(anchors.GenerateCommitHash(anchorID, myCentID, rootHash), secpIDConfig.PrivateKey)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-
-	log.Infof("Anchoring document with identifiers: [document: %#x, current: %#x, next: %#x], rootHash: %#x", document.DocumentIdentifier, document.CurrentVersion, document.NextVersion, document.DocumentRoot)
-	log.Debugf("Anchoring document with details %v", document)
-	confirmations, err := anchors.CommitAnchor(anchorID, rootHash, myCentID, [][anchors.DocumentProofLength]byte{utils.RandomByte32()}, mac)
-	if err != nil {
-		log.Error(err)
-		return centerrors.Wrap(err, "anchoring error")
-	}
-	<-confirmations
-	log.Infof("Anchored document with identifiers: [document: %#x, current: %#x, next: %#x], rootHash: %#x", document.DocumentIdentifier, document.CurrentVersion, document.NextVersion, document.DocumentRoot)
 	return nil
 }
 
