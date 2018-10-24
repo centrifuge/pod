fa741d56a669305f703b8f47e949c5eea8fd65a0
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2018-10-24 18:21:39 +0200

    remove html generation code (#372)

--- scripts/build_swagger.js
+++ scripts/build_swagger.js
@@ -1,14 +1,11 @@
-const swaggermerge = require('swagger-merge')
-const fs = require('fs')
-const path = require('path')
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const swaggermerge = require('swagger-merge');
+const fs = require('fs');
+const path = require('path');
 
-const swaggerFilesPath = path.resolve(__dirname, '../centrifuge/protobufs/gen/swagger')
-const swaggerJsonPath = path.resolve(__dirname, '../centrifuge/protobufs/gen/swagger.json')
-const swaggerHtmlPath = path.resolve(__dirname, '../centrifuge/protobufs/gen/swagger/html')
+const swaggerFilesPath = path.resolve(__dirname, '../centrifuge/protobufs/gen/swagger');
+const swaggerJsonPath = path.resolve(__dirname, '../centrifuge/protobufs/gen/swagger.json');
 const swaggerConfig = require(path.resolve(__dirname, '../swagger_config'));
-const pathPrefix = ""
+
 /* # build_swagger.js
  *
  * This script recursively searches the swaggerFilesPath for any file ending in .swagger.json
@@ -21,8 +18,8 @@ const pathPrefix = ""
 
 // Find matching swagger files in path
 // From: https://gist.github.com/kethinov/6658166
-var getSwaggerFiles = function(dir, filelist) {
-    var files = fs.readdirSync(dir);
+let getSwaggerFiles = function(dir, filelist) {
+    let files = fs.readdirSync(dir);
     filelist = filelist || [];
     files.forEach(function(file) {
         if (fs.statSync(dir + '/' + file).isDirectory()) {
@@ -37,32 +34,23 @@ var getSwaggerFiles = function(dir, filelist) {
     return filelist;
 };
 
-var files = getSwaggerFiles(swaggerFilesPath)
+let files = getSwaggerFiles(swaggerFilesPath);
 // There is a default swagger definition in swaggerConfig.defaultSwagger which we add first
-swaggerModules = [swaggerConfig.defaultSwagger,]
+swaggerModules = [swaggerConfig.defaultSwagger,];
 files.forEach(function (f) {
     swaggerModules.push(require(f))
-})
+});
 
 swaggermerge.on('warn', function (msg) {
     console.log(msg)
-})
+});
+
 swaggermerge.on('err', function (msg) {
-    console.error(msg)
-    process.exit(1)
-})
-
-var merged = swaggermerge.merge(swaggerModules, swaggerConfig.info, swaggerConfig.pathPrefix, swaggerConfig.host, swaggerConfig.schemes)
-var json = JSON.stringify(merged);
-console.log("Merged swagger.json, writing to:", swaggerJsonPath);
-fs.writeFileSync(swaggerJsonPath, json);
-
-var spectacles = exec('spectacle '+swaggerJsonPath+' -t '+swaggerHtmlPath).then(function (msg) {
-    console.log(msg['stdout'])
-    console.log("Wrote html files to: ", swaggerHtmlPath)
-    process.exit(0)
-}).catch(function (err) {
-    console.error(err)
+    console.error(msg);
     process.exit(1)
 });
 
+let merged = swaggermerge.merge(swaggerModules, swaggerConfig.info, swaggerConfig.pathPrefix, swaggerConfig.host, swaggerConfig.schemes);
+let json = JSON.stringify(merged);
+console.log("Merged swagger.json, writing to:", swaggerJsonPath);
+fs.writeFileSync(swaggerJsonPath, json);
