e7996b68b50995dd39dbe14ad596ccdf9a4a0924
Manuel Polzhofer <manuel@centrifuge.io>
2018-11-08 16:06:30 +0100

    reset integration test config

--- build/scripts/tests/run_integration_tests.sh
+++ build/scripts/tests/run_integration_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -a
 
 PARENT_DIR=`pwd`
@@ -11,8 +12,7 @@ echo "Running Integration Tests against [${CENT_ETHEREUM_NODEURL}] with TIMEOUT 
 
 status=$?
 for d in $(go list -tags=integration ./... | grep -v vendor); do
-    d="github.com/centrifuge/go-centrifuge/nft"
-    output="go test -race -coverprofile=profile.out -covermode=atomic -tags=integration $d 2>&1 -v"
+    output="go test -race -coverprofile=profile.out -covermode=atomic -tags=integration $d 2>&1"
     eval "$output"| while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done
     if [ ${PIPESTATUS[0]} -ne 0 ]; then
       status=1
