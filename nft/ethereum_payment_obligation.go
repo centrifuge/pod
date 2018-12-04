11515ba5ba679a26b46f3682981f6971f9c7ee1a
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2018-12-04 19:16:41 +0100

    NFT: fixed incorrect tokenID size (#532)
    * NFT: fixed incorrect tokenID size
    
    * better comments

--- nft/ethereum_payment_obligation.go
+++ nft/ethereum_payment_obligation.go
@@ -235,7 +235,10 @@ type MintRequest struct {
 
 // NewMintRequest converts the parameters and returns a struct with needed parameter for minting
 func NewMintRequest(to common.Address, anchorID anchors.AnchorID, proofs []*proofspb.Proof, rootHash [32]byte) (*MintRequest, error) {
-	tokenID := utils.ByteSliceToBigInt(utils.RandomSlice(256))
+
+	// tokenID is uint256 in Solidity (256 bits | max value is 2^256-1)
+	// tokenID should be random 32 bytes (32 byte = 256 bits)
+	tokenID := utils.ByteSliceToBigInt(utils.RandomSlice(32))
 	tokenURI := "http:=//www.centrifuge.io/DUMMY_URI_SERVICE"
 	proofData, err := createProofData(proofs)
 	if err != nil {
