469c0675fa76ebce212b6c54b61a98a2a9a8781d
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-12-05 12:28:14 +0100

    Add update collaborator test

--- testworld/document_consensus_test.go
+++ testworld/document_consensus_test.go
@@ -2,19 +2,20 @@
 
 package testworld
 
-import (
-	"testing"
+import "testing"
 
-	"fmt"
-)
-
-func TestHost_Happy(t *testing.T) {
+func TestHost_AddExternalCollaborator(t *testing.T) {
 	alice := doctorFord.getHost("Alice")
 	bob := doctorFord.getHost("Bob")
 	charlie := doctorFord.getHost("Charlie")
 	eAlice := alice.createHttpExpectation(t)
 	eBob := bob.createHttpExpectation(t)
 	eCharlie := charlie.createHttpExpectation(t)
+
+	a, err := alice.id()
+	if err != nil {
+		t.Error(err)
+	}
 
 	b, err := bob.id()
 	if err != nil {
@@ -25,6 +26,8 @@ func TestHost_Happy(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
+	// Alice shares invoice document with Bob first
 	res, err := alice.createInvoice(eAlice, map[string]interface{}{
 		"data": map[string]interface{}{
 			"invoice_number": "12324",
@@ -33,7 +36,7 @@ func TestHost_Happy(t *testing.T) {
 			"currency":       "GBP",
 			"net_amount":     "40",
 		},
-		"collaborators": []string{b.String(), c.String()},
+		"collaborators": []string{b.String()},
 	})
 	if err != nil {
 		t.Error(err)
@@ -46,7 +49,29 @@ func TestHost_Happy(t *testing.T) {
 		"document_id": docIdentifier,
 		"currency":    "GBP",
 	}
+	getInvoiceAndCheck(eAlice, params)
+	getInvoiceAndCheck(eBob, params)
+
+	// Bob updates invoice and shares with Charlie as well
+	res, err = bob.updateInvoice(eBob, docIdentifier, map[string]interface{}{
+		"data": map[string]interface{}{
+			"invoice_number": "12324",
+			"due_date":       "2018-09-26T23:12:37.902198664Z",
+			"gross_amount":   "40",
+			"currency":       "USD",
+			"net_amount":     "40",
+		},
+		"collaborators": []string{a.String(), c.String()},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	docIdentifier = res.Value("header").Path("$.document_id").String().NotEmpty().Raw()
+	if docIdentifier == "" {
+		t.Error("docIdentifier empty")
+	}
+	params["currency"] = "USD"
+	getInvoiceAndCheck(eAlice, params)
 	getInvoiceAndCheck(eBob, params)
 	getInvoiceAndCheck(eCharlie, params)
-	fmt.Println("Host test success")
 }
--- testworld/httputils.go
+++ testworld/httputils.go
@@ -27,13 +27,16 @@ func createInsecureClient(t *testing.T, baseURL string) *httpexpect.Expect {
 	return httpexpect.WithConfig(config)
 }
 
-func getInvoiceAndCheck(e *httpexpect.Expect, docIdentifier string, currency string) *httpexpect.Value {
+func getInvoiceAndCheck(e *httpexpect.Expect, params map[string]interface{}) *httpexpect.Value {
+	docIdentifier := params["document_id"].(string)
+
 	objGet := e.GET("/invoice/"+docIdentifier).
 		WithHeader("accept", "application/json").
 		WithHeader("Content-Type", "application/json").
 		Expect().Status(http.StatusOK).JSON().NotNull()
 	objGet.Path("$.header.document_id").String().Equal(docIdentifier)
-	objGet.Path("$.data.currency").String().Equal(currency)
+	objGet.Path("$.data.currency").String().Equal(params["currency"].(string))
+
 	return objGet
 }
 
