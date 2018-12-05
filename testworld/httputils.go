5d3ee220cc34b132ec24c3972eba00c1b3202484
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2018-12-05 12:47:20 +0100

    Add update collaborator test (#535)
    * Add update collaborator test
    
    * Add update collaborator test

--- testworld/httputils.go
+++ testworld/httputils.go
@@ -27,18 +27,30 @@ func createInsecureClient(t *testing.T, baseURL string) *httpexpect.Expect {
 	return httpexpect.WithConfig(config)
 }
 
-func getInvoiceAndCheck(e *httpexpect.Expect, docIdentifier string, currency string) *httpexpect.Value {
+func getInvoiceAndCheck(e *httpexpect.Expect, params map[string]interface{}) *httpexpect.Value {
+	docIdentifier := params["document_id"].(string)
+
 	objGet := e.GET("/invoice/"+docIdentifier).
 		WithHeader("accept", "application/json").
 		WithHeader("Content-Type", "application/json").
 		Expect().Status(http.StatusOK).JSON().NotNull()
 	objGet.Path("$.header.document_id").String().Equal(docIdentifier)
-	objGet.Path("$.data.currency").String().Equal(currency)
+	objGet.Path("$.data.currency").String().Equal(params["currency"].(string))
+
 	return objGet
 }
 
 func createInvoice(e *httpexpect.Expect, payload map[string]interface{}) *httpexpect.Object {
 	obj := e.POST("/invoice").
+		WithHeader("accept", "application/json").
+		WithHeader("Content-Type", "application/json").
+		WithJSON(payload).
+		Expect().Status(http.StatusOK).JSON().Object()
+	return obj
+}
+
+func updateInvoice(e *httpexpect.Expect, docIdentifier string, payload map[string]interface{}) *httpexpect.Object {
+	obj := e.PUT("/invoice/"+docIdentifier).
 		WithHeader("accept", "application/json").
 		WithHeader("Content-Type", "application/json").
 		WithJSON(payload).
