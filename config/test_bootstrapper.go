a1add991c7ede6183bfc0e75a5c3cab9d39b189c
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-12-12 15:39:24 +0100

    fix test

--- config/test_bootstrapper.go
+++ config/test_bootstrapper.go
@@ -7,11 +7,13 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/centrifuge/go-centrifuge/errors"
 	"github.com/centrifuge/go-centrifuge/storage"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var db *leveldb.DB
+var configdb *leveldb.DB
 
 func (*Bootstrapper) TestBootstrap(context map[string]interface{}) error {
 	// To get the config location, we need to traverse the path to find the `go-centrifuge` folder
@@ -30,9 +32,18 @@ func (*Bootstrapper) TestBootstrap(context map[string]interface{}) error {
 	cfg := LoadConfiguration(fmt.Sprintf("%s/build/configs/testing_config.yaml", match))
 	context[BootstrappedConfig] = cfg
 
+	crs := storage.GetRandomTestStoragePath()
+	cfg.SetDefault("configStorage.path", crs)
+	log.Info("Set configStorage.path to:", cfg.GetConfigStoragePath())
+	configdb, err = storage.NewLevelDBStorage(cfg.GetConfigStoragePath())
+	if err != nil {
+		return fmt.Errorf("failed to init config level db: %v", err)
+	}
+	context[BootstrappedConfigLevelDB] = configdb
+
 	rs := storage.GetRandomTestStoragePath()
-	cfg.SetDefault("storage.Path", rs)
-	log.Info("Set storage.Path to:", cfg.GetStoragePath())
+	cfg.SetDefault("storage.path", rs)
+	log.Info("Set storage.path to:", cfg.GetStoragePath())
 	db, err = storage.NewLevelDBStorage(cfg.GetStoragePath())
 	if err != nil {
 		return fmt.Errorf("failed to init level db: %v", err)
@@ -45,5 +56,16 @@ func (*Bootstrapper) TestBootstrap(context map[string]interface{}) error {
 }
 
 func (b *Bootstrapper) TestTearDown() error {
-	return db.Close()
+	var err error
+	dbs := []*leveldb.DB{db, configdb}
+	for _, idb := range dbs {
+		if ierr := idb.Close(); ierr != nil {
+			if err == nil {
+				err = errors.New("%s", ierr)
+			} else {
+				err = errors.AppendError(err, ierr)
+			}
+		}
+	}
+	return err
 }
