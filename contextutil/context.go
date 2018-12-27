03d25e1836a176ca7cf75d54338753a5306f79c4
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2018-12-27 23:03:54 +0100

    Minor

--- contextutil/context.go
+++ contextutil/context.go
@@ -8,26 +8,31 @@ import (
 	"github.com/centrifuge/go-centrifuge/identity"
 )
 
-// ErrIDConfigNotFound must be used when configuration can't be found
-const ErrIDConfigNotFound = errors.Error("id configuration wasn't found")
+type contextKey string
 
-// ErrSelfNotFound must be used when self value is not found in the context
-const ErrSelfNotFound = errors.Error("self value not found in the context")
+const (
+	// ErrIDConfigNotFound must be used when configuration can't be found
+	ErrIDConfigNotFound = errors.Error("id configuration wasn't found")
 
-type selfKey string
+	// ErrSelfNotFound must be used when self value is not found in the context
+	ErrSelfNotFound = errors.Error("self value not found in the context")
+
+	self = contextKey("self")
+)
 
 // NewCentrifugeContext creates new instance of the request headers.
+// TODO [multi-tenancy] replace config.Configuration with *configstore.TenantConfig
 func NewCentrifugeContext(ctx context.Context, config config.Configuration) (context.Context, error) {
 	idConfig, err := identity.GetIdentityConfig(config.(identity.Config))
 	if err != nil {
 		return nil, errors.NewTypedError(ErrIDConfigNotFound, errors.New("%v", err))
 	}
-	return context.WithValue(ctx, selfKey("self"), idConfig), nil
+	return context.WithValue(ctx, self, idConfig), nil
 }
 
 // Self returns Self CentID.
 func Self(ctx context.Context) (*identity.IDConfig, error) {
-	self, ok := ctx.Value(selfKey("self")).(*identity.IDConfig)
+	self, ok := ctx.Value(self).(*identity.IDConfig)
 	if ok {
 		return self, nil
 	}
