c9fee0c19b2bdbad2d61818ddd8a412c57bf7803
vedhavyas <vedhavyas.singareddi@gmail.com>
2018-12-28 10:56:40 +0100

    add tests for anchor task

--- documents/anchor_task.go
+++ documents/anchor_task.go
@@ -8,21 +8,25 @@ import (
 	"github.com/centrifuge/go-centrifuge/header"
 	"github.com/centrifuge/go-centrifuge/transactions"
 	"github.com/centrifuge/gocelery"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	logging "github.com/ipfs/go-log"
 	"github.com/satori/go.uuid"
 )
 
 const (
-	txIDParam     = "transactionID"
-	modelIDParam  = "modelID"
-	tenantIDParam = "tenantID"
+	txIDParam              = "transactionID"
+	modelIDParam           = "modelID"
+	tenantIDParam          = "tenantID"
+	documentAnchorTaskName = "Document Anchoring"
 )
 
 var log = logging.Logger("nft")
 
 type documentAnchorTask struct {
-	txID         uuid.UUID
-	id, tenantID []byte
+	txID     uuid.UUID
+	id       []byte
+	tenantID common.Address
 
 	txRepository  transactions.Repository
 	config        config.Configuration
@@ -37,15 +41,28 @@ func (d *documentAnchorTask) ParseKwargs(kwargs map[string]interface{}) error {
 		return errors.New("missing transaction ID")
 	}
 
-	d.txID = uuid.Must(uuid.FromString(txID))
-	if d.id, ok = kwargs[modelIDParam].([]byte); !ok {
+	var err error
+	d.txID, err = uuid.FromString(txID)
+	if err != nil {
+		return errors.New("invalid transaction ID")
+	}
+
+	modelID, ok := kwargs[modelIDParam].(string)
+	if !ok {
 		return errors.New("missing model ID")
 	}
 
-	if d.tenantID, ok = kwargs[tenantIDParam].([]byte); !ok {
+	d.id, err = hexutil.Decode(modelID)
+	if err != nil {
+		return errors.New("invalid model ID")
+	}
+
+	tenantID, ok := kwargs[tenantIDParam].(string)
+	if !ok {
 		return errors.New("missing tenant ID")
 	}
 
+	d.tenantID = common.HexToAddress(tenantID)
 	return nil
 }
 
@@ -62,12 +79,27 @@ func (d *documentAnchorTask) Copy() (gocelery.CeleryTask, error) {
 func (d *documentAnchorTask) RunTask() (res interface{}, err error) {
 	log.Infof("starting anchor task: %v\n", d.txID.String())
 	defer func() {
-		if err != nil {
-			log.Infof("anchor task failed: %v\n", err)
+		tx, erri := d.txRepository.Get(d.tenantID, d.txID)
+		if erri != nil {
+			log.Infof("transaction not found: %v\n", erri)
 			return
 		}
 
-		log.Infof("anchor task successful: %v\n", d.txID.String())
+		if err == nil {
+			log.Infof("anchor task successful: %v\n", d.txID.String())
+			tx.Status = transactions.Success
+			tx.Logs = append(tx.Logs, transactions.NewLog(documentAnchorTaskName, ""))
+			err = d.txRepository.Save(tx)
+			return
+		}
+
+		log.Infof("anchor task failed: %v\n", err)
+		tx.Status = transactions.Failed
+		tx.Logs = append(tx.Logs, transactions.NewLog(documentAnchorTaskName, err.Error()))
+		erri = d.txRepository.Save(tx)
+		if erri != nil {
+			err = errors.AppendError(err, erri)
+		}
 	}()
 
 	ctxh, err := header.NewContextHeader(context.Background(), d.config)
@@ -75,13 +107,13 @@ func (d *documentAnchorTask) RunTask() (res interface{}, err error) {
 		return false, errors.New("failed to get context header: %v", err)
 	}
 
-	model, err := d.modelGetFunc(d.tenantID, d.id)
+	model, err := d.modelGetFunc(d.tenantID[:], d.id)
 	if err != nil {
 		return false, errors.New("failed to get model: %v", err)
 	}
 
 	if _, err = AnchorDocument(ctxh, model, d.processor, func(id []byte, model Model) error {
-		return d.modelSaveFunc(d.tenantID, id, model)
+		return d.modelSaveFunc(d.tenantID[:], id, model)
 	}); err != nil {
 		return false, errors.New("failed to anchor document: %v", err)
 	}
---
+++ documents/anchor_task_test.go
@@ -0,0 +1,86 @@
+// +build unit
+
+package documents
+
+import (
+	"testing"
+
+	cc "github.com/centrifuge/go-centrifuge/common"
+	"github.com/centrifuge/go-centrifuge/utils"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDocumentAnchorTask_ParseKwargs(t *testing.T) {
+	tests := []struct {
+		name   string
+		kwargs map[string]interface{}
+		err    string
+	}{
+		{
+			name: "nil kwargs",
+			err:  "missing transaction ID",
+		},
+
+		{
+			kwargs: map[string]interface{}{
+				txIDParam: "some string",
+			},
+			err: "invalid transaction ID",
+		},
+
+		// missing model ID
+		{
+			kwargs: map[string]interface{}{
+				txIDParam: uuid.Must(uuid.NewV4()).String(),
+			},
+			err: "missing model ID",
+		},
+
+		// missing tenantID
+		{
+			kwargs: map[string]interface{}{
+				txIDParam:    uuid.Must(uuid.NewV4()).String(),
+				modelIDParam: hexutil.Encode(utils.RandomSlice(32)),
+			},
+
+			err: "missing tenant ID",
+		},
+
+		// all good
+		{
+			name: "success",
+			kwargs: map[string]interface{}{
+				txIDParam:     uuid.Must(uuid.NewV4()).String(),
+				modelIDParam:  hexutil.Encode(utils.RandomSlice(32)),
+				tenantIDParam: cc.DummyIdentity,
+			},
+		},
+	}
+
+	for _, c := range tests {
+		name := c.name
+		if name == "" {
+			name = c.err
+		}
+
+		t.Run(name, func(t *testing.T) {
+			d, err := utils.SimulateJSONDecodeForGocelery(c.kwargs)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			task := new(documentAnchorTask)
+			err = task.ParseKwargs(d)
+			if c.err == "" {
+				assert.Equal(t, task.txID.String(), c.kwargs[txIDParam])
+				assert.Equal(t, hexutil.Encode(task.id), c.kwargs[modelIDParam])
+				assert.Equal(t, task.tenantID, c.kwargs[tenantIDParam])
+				return
+			}
+
+			assert.EqualError(t, err, c.err)
+		})
+	}
+}
