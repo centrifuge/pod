0956e7a4219584f14902c282e9b39f50909f12f1
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-02-13 14:14:06 +0100

    Compact Precise Proofs (#743)
    * latest precise proofs
    
    * latest precise proofs
    
    * using compact properties
    
    * added compact prefix
    
    * fix client test
    
    * fix client test

--- documents/handler.go
+++ documents/handler.go
@@ -104,7 +104,7 @@ func ConvertProofsToClientFormat(proofs []*proofspb.Proof) []*documentpb.Proof {
 // ConvertProofToClientFormat converts a proof in precise proof format in to a client protobuf proof
 func ConvertProofToClientFormat(proof *proofspb.Proof) *documentpb.Proof {
 	return &documentpb.Proof{
-		Property:     proof.GetReadableName(),
+		Property:     hexutil.Encode(proof.GetCompactName()),
 		Value:        hexutil.Encode(proof.Value),
 		Salt:         hexutil.Encode(proof.Salt),
 		Hash:         hexutil.Encode(proof.Hash),
