95697789003ec4d3d40b5d959ecdec97ad42cf33
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-02-14 16:29:47 +0100

    moved did service interface

--- identity/ideth/service.go
+++ identity/ideth/service.go
@@ -26,64 +26,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-// DID stores the identity address of the user
-type DID common.Address
-
-// ToAddress returns the DID as common.Address
-func (d DID) ToAddress() common.Address {
-	return common.Address(d)
-}
-
-// NewDID returns a DID based on a common.Address
-func NewDID(address common.Address) DID {
-	return DID(address)
-}
-
-// NewDIDFromString returns a DID based on a hex string
-func NewDIDFromString(address string) DID {
-	return DID(common.HexToAddress(address))
-}
-
-// Service interface contains the methods to interact with the identity contract
-type Service interface {
-	// AddKey adds a key to identity contract
-	AddKey(ctx context.Context, key Key) error
-
-	// GetKey return a key from the identity contract
-	GetKey(did DID, key [32]byte) (*KeyResponse, error)
-
-	// RawExecute calls the execute method on the identity contract
-	RawExecute(ctx context.Context, to common.Address, data []byte) error
-
-	// Execute creates the abi encoding an calls the execute method on the identity contract
-	Execute(ctx context.Context, to common.Address, contractAbi, methodName string, args ...interface{}) error
-
-	// IsSignedWithPurpose verifies if a message is signed with one of the identities specific purpose keys
-	IsSignedWithPurpose(did DID, message [32]byte, _signature []byte, _purpose *big.Int) (bool, error)
-
-	// AddMultiPurposeKey adds a key with multiple purposes
-	AddMultiPurposeKey(context context.Context, key [32]byte, purposes []*big.Int, keyType *big.Int) error
-
-	// RevokeKey revokes an existing key in the smart contract
-	RevokeKey(ctx context.Context, key [32]byte) error
-
-	// GetClientP2PURL returns the p2p url associated with the did
-	GetClientP2PURL(did DID) (string, error)
-
-	//Exists checks if an identity contract exists
-	Exists(ctx context.Context, did DID) error
-
-	// ValidateKey checks if a given key is valid for the given centrifugeID.
-	ValidateKey(ctx context.Context, did DID, key []byte, purpose int64) error
-
-	// GetClientsP2PURLs returns p2p urls associated with each centIDs
-	// will error out at first failure
-	GetClientsP2PURLs(did []*DID) ([]string, error)
-
-	// GetKeysByPurpose returns keys grouped by purpose from the identity contract.
-	GetKeysByPurpose(did DID, purpose *big.Int) ([][32]byte, error)
-}
-
 type contract interface {
 
 	// Ethereum Calls
