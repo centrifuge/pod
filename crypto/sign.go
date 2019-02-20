42968b31c1abf41bc9c7f2eaa853a376ee8ad42d
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2019-02-20 12:16:17 +0100

    hash instead of copying the message (#759)
    Hash the message before signing

--- crypto/sign.go
+++ crypto/sign.go
@@ -17,13 +17,11 @@ func SignMessage(privateKey, message []byte, curveType string, ethereumSign bool
 	curveType = strings.ToLower(curveType)
 	switch curveType {
 	case CurveSecp256K1:
-		msg := make([]byte, MaxMsgLen)
-		copy(msg, message)
 		if ethereumSign {
-			return secp256k1.SignEthereum(msg, privateKey)
+			return secp256k1.SignEthereum(message, privateKey)
 		}
 
-		return secp256k1.Sign(msg, privateKey)
+		return secp256k1.Sign(message, privateKey)
 	case CurveEd25519:
 		return nil, errors.New("curve ed25519 not supported yet")
 	default:
