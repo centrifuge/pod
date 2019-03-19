c42a355e11bdcb2d7d9c32688f3bc656ff79414d
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-03-19 17:31:46 +0100

    add convertors

---
+++ documents/invoice/convertors.go
@@ -0,0 +1,426 @@
+package invoice
+
+import (
+	"strings"
+
+	"github.com/centrifuge/centrifuge-protobufs/gen/go/invoice"
+	"github.com/centrifuge/go-centrifuge/documents"
+	"github.com/centrifuge/go-centrifuge/identity"
+	clientpb "github.com/centrifuge/go-centrifuge/protobufs/gen/go/invoice"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+func toClientAttachments(atts []*BinaryAttachment) []*clientpb.BinaryAttachment {
+	catts := make([]*clientpb.BinaryAttachment, len(atts), len(atts))
+	for i, att := range atts {
+		var data, checksum string
+		if len(att.Data) > 0 {
+			data = hexutil.Encode(att.Data)
+		}
+
+		if len(att.Checksum) > 0 {
+			checksum = hexutil.Encode(att.Checksum)
+		}
+
+		catts[i] = &clientpb.BinaryAttachment{
+			Name:     att.Name,
+			FileType: att.FileType,
+			Size:     att.Size,
+			Data:     data,
+			Checksum: checksum,
+		}
+	}
+
+	return catts
+}
+
+func toP2PAttachments(atts []*BinaryAttachment) []*invoicepb.BinaryAttachment {
+	patts := make([]*invoicepb.BinaryAttachment, len(atts), len(atts))
+	for i, att := range atts {
+		patts[i] = &invoicepb.BinaryAttachment{
+			Name:     att.Name,
+			FileType: att.FileType,
+			Size:     att.Size,
+			Data:     att.Data,
+			Checksum: att.Checksum,
+		}
+	}
+
+	return patts
+}
+
+func fromClientAttachments(catts []*clientpb.BinaryAttachment) (atts []*BinaryAttachment, err error) {
+	atts = make([]*BinaryAttachment, len(catts), len(catts))
+	for i, att := range catts {
+		var data, checksum []byte
+		if s := strings.TrimSpace(att.Data); s != "" {
+			data, err = hexutil.Decode(s)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if s := strings.TrimSpace(att.Checksum); s != "" {
+			checksum, err = hexutil.Decode(s)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		atts[i] = &BinaryAttachment{
+			Name:     att.Name,
+			FileType: att.FileType,
+			Size:     att.Size,
+			Data:     data,
+			Checksum: checksum,
+		}
+	}
+
+	return atts, err
+}
+
+func fromP2PAttachments(patts []*invoicepb.BinaryAttachment) []*BinaryAttachment {
+	atts := make([]*BinaryAttachment, len(patts), len(patts))
+	for i, att := range patts {
+		atts[i] = &BinaryAttachment{
+			Name:     att.Name,
+			FileType: att.FileType,
+			Size:     att.Size,
+			Data:     att.Data,
+			Checksum: att.Checksum,
+		}
+	}
+
+	return atts
+}
+
+func toClientLineItems(items []*LineItem) []*clientpb.InvoiceLineItem {
+	citems := make([]*clientpb.InvoiceLineItem, len(items), len(items))
+	for i, item := range items {
+		decs := documents.DecimalsToStrings(
+			item.PricePerUnit, item.Quantity, item.NetWeight, item.TaxAmount, item.TaxRate, item.TaxCode, item.TotalAmount)
+
+		citems[i] = &clientpb.InvoiceLineItem{
+			ItemNumber:              item.ItemNumber,
+			Description:             item.Description,
+			SenderPartNo:            item.SenderPartNo,
+			PricePerUnit:            decs[0],
+			Quantity:                decs[1],
+			UnitOfMeasure:           item.UnitOfMeasure,
+			NetWeight:               decs[2],
+			TaxAmount:               decs[3],
+			TaxRate:                 decs[4],
+			TaxCode:                 decs[5],
+			TotalAmount:             decs[6],
+			PurchaseOrderNumber:     item.PurchaseOrderNumber,
+			PurchaseOrderItemNumber: item.PurchaseOrderItemNumber,
+			DeliveryNoteNumber:      item.DeliveryNoteNumber,
+		}
+	}
+
+	return citems
+}
+
+func toP2PLineItems(items []*LineItem) ([]*invoicepb.InvoiceLineItem, error) {
+	pitems := make([]*invoicepb.InvoiceLineItem, len(items), len(items))
+	for i, item := range items {
+		decs, err := documents.DecimalsToBytes(
+			item.PricePerUnit, item.Quantity, item.NetWeight, item.TaxAmount, item.TaxRate, item.TaxCode, item.TotalAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		pitems[i] = &invoicepb.InvoiceLineItem{
+			ItemNumber:              item.ItemNumber,
+			Description:             item.Description,
+			SenderPartNo:            item.SenderPartNo,
+			PricePerUnit:            decs[0],
+			Quantity:                decs[1],
+			UnitOfMeasure:           item.UnitOfMeasure,
+			NetWeight:               decs[2],
+			TaxAmount:               decs[3],
+			TaxRate:                 decs[4],
+			TaxCode:                 decs[5],
+			TotalAmount:             decs[6],
+			PurchaseOrderNumber:     item.PurchaseOrderNumber,
+			PurchaseOrderItemNumber: item.PurchaseOrderItemNumber,
+			DeliveryNoteNumber:      item.DeliveryNoteNumber,
+		}
+	}
+
+	return pitems, nil
+}
+
+func fromClientLineItems(citems []*clientpb.InvoiceLineItem) ([]*LineItem, error) {
+	items := make([]*LineItem, len(citems), len(citems))
+	for i, item := range citems {
+		decs, err := documents.StringsToDecimals(
+			item.PricePerUnit, item.Quantity, item.NetWeight, item.TaxAmount, item.TaxRate, item.TaxCode, item.TotalAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = &LineItem{
+			ItemNumber:              item.ItemNumber,
+			Description:             item.Description,
+			SenderPartNo:            item.SenderPartNo,
+			PricePerUnit:            decs[0],
+			Quantity:                decs[1],
+			UnitOfMeasure:           item.UnitOfMeasure,
+			NetWeight:               decs[2],
+			TaxAmount:               decs[3],
+			TaxRate:                 decs[4],
+			TaxCode:                 decs[5],
+			TotalAmount:             decs[6],
+			PurchaseOrderNumber:     item.PurchaseOrderNumber,
+			PurchaseOrderItemNumber: item.PurchaseOrderItemNumber,
+			DeliveryNoteNumber:      item.DeliveryNoteNumber,
+		}
+	}
+
+	return items, nil
+}
+
+func fromP2PLineItems(pitems []*invoicepb.InvoiceLineItem) ([]*LineItem, error) {
+	items := make([]*LineItem, len(pitems), len(pitems))
+	for i, item := range pitems {
+		decs, err := documents.BytesToDecimals(
+			item.PricePerUnit, item.Quantity, item.NetWeight, item.TaxAmount, item.TaxRate, item.TaxCode, item.TotalAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = &LineItem{
+			ItemNumber:              item.ItemNumber,
+			Description:             item.Description,
+			SenderPartNo:            item.SenderPartNo,
+			PricePerUnit:            decs[0],
+			Quantity:                decs[1],
+			UnitOfMeasure:           item.UnitOfMeasure,
+			NetWeight:               decs[2],
+			TaxAmount:               decs[3],
+			TaxRate:                 decs[4],
+			TaxCode:                 decs[5],
+			TotalAmount:             decs[6],
+			PurchaseOrderNumber:     item.PurchaseOrderNumber,
+			PurchaseOrderItemNumber: item.PurchaseOrderItemNumber,
+			DeliveryNoteNumber:      item.DeliveryNoteNumber,
+		}
+	}
+
+	return items, nil
+}
+
+func toClientPaymentDetails(details []*PaymentDetails) []*clientpb.PaymentDetails {
+	cdetails := make([]*clientpb.PaymentDetails, len(details), len(details))
+	for i, detail := range details {
+		decs := documents.DecimalsToStrings(detail.Amount)
+		dids := identity.DIDsToStrings(detail.Payee, detail.Payer)
+		cdetails[i] = &clientpb.PaymentDetails{
+			Id:                    detail.ID,
+			DateExecuted:          detail.DateExecuted,
+			Payee:                 dids[0],
+			Payer:                 dids[1],
+			Amount:                decs[0],
+			Currency:              detail.Currency,
+			Reference:             detail.Reference,
+			BankName:              detail.BankName,
+			BankAddress:           detail.BankAddress,
+			BankAccountCurrency:   detail.BankAccountCurrency,
+			BankAccountHolderName: detail.BankAccountHolderName,
+			BankAccountNumber:     detail.BankAccountNumber,
+			BankCountry:           detail.BankCountry,
+			BankKey:               detail.BankKey,
+			CryptoChainUri:        detail.CryptoChainURI,
+			CryptoFrom:            detail.CryptoFrom,
+			CryptoTo:              detail.CryptoTo,
+			CryptoTransactionId:   detail.CryptoTransactionID,
+		}
+	}
+
+	return cdetails
+}
+
+func toP2PPaymentDetails(details []*PaymentDetails) ([]*invoicepb.PaymentDetails, error) {
+	pdetails := make([]*invoicepb.PaymentDetails, len(details), len(details))
+	for i, detail := range details {
+		decs, err := documents.DecimalsToBytes(detail.Amount)
+		if err != nil {
+			return nil, err
+		}
+		dids := identity.DIDsToBytes(detail.Payee, detail.Payer)
+		pdetails[i] = &invoicepb.PaymentDetails{
+			Id:                    detail.ID,
+			DateExecuted:          detail.DateExecuted,
+			Payee:                 dids[0],
+			Payer:                 dids[1],
+			Amount:                decs[0],
+			Currency:              detail.Currency,
+			Reference:             detail.Reference,
+			BankName:              detail.BankName,
+			BankAddress:           detail.BankAddress,
+			BankAccountCurrency:   detail.BankAccountCurrency,
+			BankAccountHolderName: detail.BankAccountHolderName,
+			BankAccountNumber:     detail.BankAccountNumber,
+			BankCountry:           detail.BankCountry,
+			BankKey:               detail.BankKey,
+			CryptoChainUri:        detail.CryptoChainURI,
+			CryptoFrom:            detail.CryptoFrom,
+			CryptoTo:              detail.CryptoTo,
+			CryptoTransactionId:   detail.CryptoTransactionID,
+		}
+	}
+
+	return pdetails, nil
+}
+
+func fromClientPaymentDetails(cdetails []*clientpb.PaymentDetails) ([]*PaymentDetails, error) {
+	details := make([]*PaymentDetails, len(cdetails), len(cdetails))
+	for i, detail := range cdetails {
+		decs, err := documents.StringsToDecimals(detail.Amount)
+		if err != nil {
+			return nil, err
+		}
+
+		dids, err := identity.StringsToDIDs(detail.Payee, detail.Payer)
+		if err != nil {
+			return nil, err
+		}
+
+		details[i] = &PaymentDetails{
+			ID:                    detail.Id,
+			DateExecuted:          detail.DateExecuted,
+			Payee:                 dids[0],
+			Payer:                 dids[1],
+			Amount:                decs[0],
+			Currency:              detail.Currency,
+			Reference:             detail.Reference,
+			BankName:              detail.BankName,
+			BankAddress:           detail.BankAddress,
+			BankAccountCurrency:   detail.BankAccountCurrency,
+			BankAccountHolderName: detail.BankAccountHolderName,
+			BankAccountNumber:     detail.BankAccountNumber,
+			BankCountry:           detail.BankCountry,
+			BankKey:               detail.BankKey,
+			CryptoChainURI:        detail.CryptoChainUri,
+			CryptoFrom:            detail.CryptoFrom,
+			CryptoTo:              detail.CryptoTo,
+			CryptoTransactionID:   detail.CryptoTransactionId,
+		}
+	}
+
+	return details, nil
+}
+
+func fromP2PPaymentDetails(pdetails []*invoicepb.PaymentDetails) ([]*PaymentDetails, error) {
+	details := make([]*PaymentDetails, len(pdetails), len(pdetails))
+	for i, detail := range pdetails {
+		decs, err := documents.BytesToDecimals(detail.Amount)
+		if err != nil {
+			return nil, err
+		}
+		dids := identity.BytesToDIDs(detail.Payee, detail.Payer)
+		details[i] = &PaymentDetails{
+			ID:                    detail.Id,
+			DateExecuted:          detail.DateExecuted,
+			Payee:                 dids[0],
+			Payer:                 dids[1],
+			Amount:                decs[0],
+			Currency:              detail.Currency,
+			Reference:             detail.Reference,
+			BankName:              detail.BankName,
+			BankAddress:           detail.BankAddress,
+			BankAccountCurrency:   detail.BankAccountCurrency,
+			BankAccountHolderName: detail.BankAccountHolderName,
+			BankAccountNumber:     detail.BankAccountNumber,
+			BankCountry:           detail.BankCountry,
+			BankKey:               detail.BankKey,
+			CryptoChainURI:        detail.CryptoChainUri,
+			CryptoFrom:            detail.CryptoFrom,
+			CryptoTo:              detail.CryptoTo,
+			CryptoTransactionID:   detail.CryptoTransactionId,
+		}
+	}
+
+	return details, nil
+}
+
+func toClientTaxItems(items []*TaxItem) []*clientpb.InvoiceTaxItem {
+	citems := make([]*clientpb.InvoiceTaxItem, len(items), len(items))
+	for i, item := range items {
+		decs := documents.DecimalsToStrings(item.TaxAmount, item.TaxRate, item.TaxCode, item.TaxBaseAmount)
+		citems[i] = &clientpb.InvoiceTaxItem{
+			ItemNumber:        item.ItemNumber,
+			InvoiceItemNumber: item.InvoiceItemNumber,
+			TaxAmount:         decs[0],
+			TaxRate:           decs[1],
+			TaxCode:           decs[2],
+			TaxBaseAmount:     decs[3],
+		}
+	}
+
+	return citems
+}
+
+func toP2PTaxItems(items []*TaxItem) ([]*invoicepb.InvoiceTaxItem, error) {
+	pitems := make([]*invoicepb.InvoiceTaxItem, len(items), len(items))
+	for i, item := range items {
+		decs, err := documents.DecimalsToBytes(item.TaxAmount, item.TaxRate, item.TaxCode, item.TaxBaseAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		pitems[i] = &invoicepb.InvoiceTaxItem{
+			ItemNumber:        item.ItemNumber,
+			InvoiceItemNumber: item.InvoiceItemNumber,
+			TaxAmount:         decs[0],
+			TaxRate:           decs[1],
+			TaxCode:           decs[2],
+			TaxBaseAmount:     decs[3],
+		}
+	}
+
+	return pitems, nil
+}
+
+func fromClientTaxItems(citems []*clientpb.InvoiceTaxItem) ([]*TaxItem, error) {
+	items := make([]*TaxItem, len(citems), len(citems))
+	for i, item := range citems {
+		decs, err := documents.StringsToDecimals(item.TaxAmount, item.TaxRate, item.TaxCode, item.TaxBaseAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = &TaxItem{
+			ItemNumber:        item.ItemNumber,
+			InvoiceItemNumber: item.InvoiceItemNumber,
+			TaxAmount:         decs[0],
+			TaxRate:           decs[1],
+			TaxCode:           decs[2],
+			TaxBaseAmount:     decs[3],
+		}
+	}
+
+	return items, nil
+}
+
+func fromP2PTaxItems(pitems []*invoicepb.InvoiceTaxItem) ([]*TaxItem, error) {
+	items := make([]*TaxItem, len(pitems), len(pitems))
+	for i, item := range pitems {
+		decs, err := documents.BytesToDecimals(item.TaxAmount, item.TaxRate, item.TaxCode, item.TaxBaseAmount)
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = &TaxItem{
+			ItemNumber:        item.ItemNumber,
+			InvoiceItemNumber: item.InvoiceItemNumber,
+			TaxAmount:         decs[0],
+			TaxRate:           decs[1],
+			TaxCode:           decs[2],
+			TaxBaseAmount:     decs[3],
+		}
+	}
+
+	return items, nil
+}
---
+++ identity/convertors.go
@@ -0,0 +1,64 @@
+package identity
+
+import "strings"
+
+// StringsToDIDs converts hex strings to DIDs.
+func StringsToDIDs(strs ...string) ([]*DID, error) {
+	dids := make([]*DID, len(strs), len(strs))
+
+	for i, str := range strs {
+		str = strings.TrimSpace(str)
+		if str == "" {
+			continue
+		}
+
+		did, err := NewDIDFromString(str)
+		if err != nil {
+			return nil, err
+		}
+
+		dids[i] = &did
+	}
+
+	return dids, nil
+}
+
+// DIDsToStrings converts DIDs to hex strings.
+func DIDsToStrings(dids ...*DID) []string {
+	strs := make([]string, len(dids), len(dids))
+
+	for i, did := range dids {
+		if did == nil {
+			continue
+		}
+
+		strs[i] = did.String()
+	}
+
+	return strs
+}
+
+// DIDsToBytes converts DIDs to bytes.
+func DIDsToBytes(dids ...*DID) [][]byte {
+	bytes := make([][]byte, len(dids), len(dids))
+	for i, did := range dids {
+		if did == nil {
+			continue
+		}
+
+		bytes[i] = did[:]
+	}
+
+	return bytes
+}
+
+// BytesToDIDs converts bytes to DIDs
+func BytesToDIDs(bytes ...[]byte) []*DID {
+	dids := make([]*DID, len(bytes), len(bytes))
+	for i, bs := range bytes {
+		did := NewDIDFromBytes(bs)
+		dids[i] = &did
+	}
+
+	return dids
+}
