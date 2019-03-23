bf936826e59b385f398328e0823a19e76ab27fa6
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-03-24 00:42:22 +0100

    Merge upstream

--- documents/invoice/model.go
+++ documents/invoice/model.go
@@ -629,9 +629,6 @@ func (i *Invoice) getDocumentDataTree() (tree *proofs.DocumentTree, err error) {
 
 // CreateProofs generates proofs for given fields.
 func (i *Invoice) CreateProofs(fields []string) (proofs []*proofspb.Proof, err error) {
-	if i.DataModified || i.SignaturesModified || i.CoreDocModified {
-		panic("Bloody hell")
-	}
 	tree, err := i.getDocumentDataTree()
 	if err != nil {
 		return nil, errors.New("createProofs error %v", err)
