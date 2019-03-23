c91baba962b401b75adbbd48c9d0f12645a58b94
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-03-23 11:39:05 +0100

    entity: added model + api (#867)
    * added files for entity
    
    * added rest api for entity
    
    * added first simple entity model tests
    
    * added entity model tests
    
    * removed unused files
    
    * formatting
    
    * delete old folder
    
    * updated protobuf for entity
    
    * changed to did for identity
    
    * comment address boolean
    
    * fixed read acl tests
    
    * latest protobuf version
    
    * correct protobuf build
    
    * updated swagger
    
    * swagger updated

---
+++ testingutils/documents/entity.go
@@ -0,0 +1,36 @@
+package testingdocuments
+
+import (
+	"github.com/centrifuge/centrifuge-protobufs/gen/go/entity"
+	cliententitypb "github.com/centrifuge/go-centrifuge/protobufs/gen/go/entity"
+	"github.com/centrifuge/go-centrifuge/testingutils/identity"
+)
+
+func CreateEntityData() entitypb.Entity {
+	return entitypb.Entity{
+		Identity: testingidentity.GenerateRandomDID().ToAddress().Bytes(),
+		LegalName:"Company Test",
+		Contacts: []*entitypb.Contact{{Name:"Satoshi Nakamoto"}},
+		Addresses:[]*entitypb.Address{{IsMain:true,
+		AddressLine1:"Sample Street 1",
+		Zip:"12345",
+		State:"Germany",
+},{IsMain:false,State:"US"}},
+	}
+}
+
+func CreateEntityPayload() *cliententitypb.EntityCreatePayload {
+	return &cliententitypb.EntityCreatePayload{
+		Data: &cliententitypb.EntityData{
+			Identity: testingidentity.GenerateRandomDID().ToAddress().String(),
+			LegalName:"Company Test",
+			Contacts: []*entitypb.Contact{{Name:"Satoshi Nakamoto"}},
+			Addresses:[]*entitypb.Address{{IsMain:true,
+				AddressLine1:"Sample Street 1",
+				Zip:"12345",
+				State:"Germany",
+			},{IsMain:false,State:"US"}},
+		},
+		Collaborators: []string{testingidentity.GenerateRandomDID().String()},
+	}
+}
