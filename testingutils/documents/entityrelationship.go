63f39eb005d9699915e191c090bacae180130107
mikiquantum <miguel.hervas.lazaro@gmail.com>
2019-04-02 16:35:27 +0200

    dep

--- testingutils/documents/entityrelationship.go
+++ testingutils/documents/entityrelationship.go
@@ -11,7 +11,7 @@ func CreateEntityRelationshipData() entitypb.EntityRelationship {
 	did2, _ := identity.NewDIDFromString("0x5F9132e0F92952abCb154A9b34563891ffe1AAcb")
 	return entitypb.EntityRelationship{
 		OwnerIdentity:  did[:],
-		TargetIdentity:  did2[:],
+		TargetIdentity: did2[:],
 	}
 }
 
@@ -19,8 +19,7 @@ func CreateEntityRelationshipPayload() *cliententitypb.EntityRelationshipCreateP
 	return &cliententitypb.EntityRelationshipCreatePayload{
 		Data: &cliententitypb.EntityRelationshipData{
 			OwnerIdentity:  "0xed03Fa80291fF5DDC284DE6b51E716B130b05e20",
-			TargetIdentity:  "0x5F9132e0F92952abCb154A9b34563891ffe1AAcb",
+			TargetIdentity: "0x5F9132e0F92952abCb154A9b34563891ffe1AAcb",
 		},
-
 	}
 }
