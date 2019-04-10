76dbc3b1a5931d2374a3342609d266a948792956
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-04-10 18:04:09 +0200

    fmt

--- protobufs/gen/go/nft/service.pb.go
+++ protobufs/gen/go/nft/service.pb.go
@@ -6,11 +6,12 @@ package nftpb
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger/options"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
