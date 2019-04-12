8a56f339547c8fcab28010ebc97ee3e0c36a5138
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-04-12 18:37:55 +0200

    add mock processor

---
+++ testingutils/commons/mock_processor.go
@@ -0,0 +1,23 @@
+// +build unit integration
+
+package testingcommons
+
+import (
+	"context"
+	"github.com/centrifuge/centrifuge-protobufs/gen/go/p2p"
+	"github.com/centrifuge/go-centrifuge/identity"
+	"github.com/stretchr/testify/mock"
+)
+
+
+type MockRequestProcessor struct {
+	mock.Mock
+}
+
+func (m *MockRequestProcessor)RequestDocumentWithAccessToken(ctx context.Context, granterDID identity.DID, tokenIdentifier,
+	entityIdentifier, entityRelationIdentifier []byte) (*p2ppb.GetDocumentResponse, error) {
+	args := m.Called(granterDID,tokenIdentifier,entityIdentifier,entityRelationIdentifier)
+	resp, _ := args.Get(0).(*p2ppb.GetDocumentResponse)
+	return resp, args.Error(1)
+}
+
