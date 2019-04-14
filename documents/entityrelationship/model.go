28f5e4cf1116e49a2b6bb359f8902901924b5079
Charleen Fei <fei.charleen@gmail.com>
2019-04-14 21:56:56 +0200

    wip

--- documents/entityrelationship/model.go
+++ documents/entityrelationship/model.go
@@ -5,10 +5,9 @@ import (
 	"encoding/json"
 	"reflect"
 
-	"github.com/centrifuge/centrifuge-protobufs/gen/go/entity"
-
 	"github.com/centrifuge/centrifuge-protobufs/documenttypes"
 	"github.com/centrifuge/centrifuge-protobufs/gen/go/coredocument"
+	"github.com/centrifuge/centrifuge-protobufs/gen/go/entity"
 	"github.com/centrifuge/go-centrifuge/documents"
 	"github.com/centrifuge/go-centrifuge/errors"
 	"github.com/centrifuge/go-centrifuge/identity"
