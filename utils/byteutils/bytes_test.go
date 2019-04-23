03dec382d264d1277886a70e1c63183e65ec4711
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-04-23 17:15:01 +0200

    more testst

--- utils/byteutils/bytes_test.go
+++ utils/byteutils/bytes_test.go
@@ -133,3 +133,156 @@ func TestIntBytesFromString(t *testing.T) {
 		assert.Equal(t, c.res, res)
 	}
 }
+
+func TestIsBitSet(t *testing.T) {
+	tests := []struct {
+		name   string
+		b      byte
+		p      uint
+		expect bool
+	}{
+		{
+			"all 1s, pos 0",
+			byte(255),
+			0,
+			true,
+		},
+		{
+			"all 1s, pos 1",
+			byte(255),
+			1,
+			true,
+		},
+		{
+			"all 1s, pos 7",
+			byte(255),
+			7,
+			true,
+		},
+		{
+			"pos[1] = 1, pos 1",
+			byte(2),
+			1,
+			true,
+		},
+		{
+			"pos[1] = 1, pos 2",
+			byte(2),
+			2,
+			false,
+		},
+		{
+			"all 0s, pos 0",
+			byte(0),
+			0,
+			false,
+		},
+		{
+			"all 0s, pos 1",
+			byte(0),
+			1,
+			false,
+		},
+		{
+			"all 0s, pos 7",
+			byte(0),
+			7,
+			false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expect, IsBitSet(test.b, test.p))
+		})
+	}
+}
+
+func TestSetBit(t *testing.T) {
+	tests := []struct {
+		name   string
+		b      byte
+		p      uint
+		expect byte
+	}{
+		{
+			"val = 1, set pos = 1",
+			byte(1),
+			1,
+			3,
+		},
+		{
+			"val = 1, set pos = 2",
+			byte(1),
+			2,
+			5,
+		},
+		{
+			"val = 2, set pos = 2",
+			byte(2),
+			2,
+			6,
+		},
+		{
+			"val = 254, set pos = 0",
+			byte(254),
+			0,
+			255,
+		},
+		{
+			"val = 254, set pos = 8",
+			byte(254),
+			9,
+			254,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expect, SetBit(test.b, test.p))
+		})
+	}
+}
+
+func TestClearBit(t *testing.T) {
+	tests := []struct {
+		name   string
+		b      byte
+		p      uint
+		expect byte
+	}{
+		{
+			"val = 1, clear pos = 1",
+			byte(1),
+			1,
+			1,
+		},
+		{
+			"val = 1, clear pos = 0",
+			byte(1),
+			0,
+			0,
+		},
+		{
+			"val = 2, clear pos = 1",
+			byte(2),
+			1,
+			0,
+		},
+		{
+			"val = 255, clear pos = 0",
+			byte(255),
+			0,
+			254,
+		},
+		{
+			"val = 254, clear pos = 8",
+			byte(255),
+			9,
+			255,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expect, ClearBit(test.b, test.p))
+		})
+	}
+}
