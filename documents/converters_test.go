ff48c8587ea47629847c5b2fa60f7404e9ab7162
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-05-22 18:21:45 +0530

    fix empty collabs

--- documents/converters_test.go
+++ documents/converters_test.go
@@ -102,11 +102,11 @@ func TestCollaboratorAccess(t *testing.T) {
 	id2 := testingidentity.GenerateRandomDID()
 	id3 := testingidentity.GenerateRandomDID()
 	rcs := &documentpb.ReadAccess{
-		Collaborators: []string{id1.String(), id2.String()},
+		Collaborators: []string{id1.String(), id2.String(), ""},
 	}
 
 	wcs := &documentpb.WriteAccess{
-		Collaborators: []string{id2.String(), id3.String()},
+		Collaborators: []string{id2.String(), id3.String(), ""},
 	}
 
 	ca, err := FromClientCollaboratorAccess(rcs, wcs)
