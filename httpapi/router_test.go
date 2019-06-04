6d08b0d850971f96ae759f9fee3164b6713f2641
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-06-04 12:36:09 +0200

    add middleware tests

---
+++ httpapi/router_test.go
@@ -0,0 +1,61 @@
+// +build unit
+
+package httpapi
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/centrifuge/go-centrifuge/config"
+	testingidentity "github.com/centrifuge/go-centrifuge/testingutils/identity"
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouter_auth(t *testing.T) {
+	cctx := &chi.Context{RoutePath: "/documents"}
+	ctx := context.WithValue(context.Background(), chi.RouteCtxKey, cctx)
+	// missing auth
+	r := httptest.NewRequest("POST", "/documents", nil).WithContext(ctx)
+	w := httptest.NewRecorder()
+	h := auth(nil)
+	h.ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusForbidden)
+	assert.Contains(t, w.Body.String(), "'authorization' header missing")
+
+	// ping
+	cctx.RoutePath = "/ping"
+	r = httptest.NewRequest("POST", "/ping", nil).WithContext(ctx)
+	w = httptest.NewRecorder()
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		v := ctx.Value(config.AccountHeaderKey)
+		assert.Nil(t, v)
+		w.WriteHeader(http.StatusOK)
+	})
+	auth(next).ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusOK)
+
+	// success
+	cctx.RoutePath = "/documents"
+	did := testingidentity.GenerateRandomDID()
+	r = httptest.NewRequest("POST", "/documents", nil).WithContext(ctx)
+	r.Header.Set("authorization", did.String())
+	w = httptest.NewRecorder()
+	next = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		v := ctx.Value(config.AccountHeaderKey).(string)
+		assert.Equal(t, did.String(), v)
+		w.WriteHeader(http.StatusOK)
+	})
+	auth(next).ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusOK)
+}
+
+func TestRouter(t *testing.T) {
+	r := Router(nil, nil, nil)
+	assert.Len(t, r.Middlewares(), 4)
+	assert.Len(t, r.Routes(), 2)
+}
