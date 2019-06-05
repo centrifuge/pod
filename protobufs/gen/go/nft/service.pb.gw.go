ee221b41f56b2a9faae5173cfd3255d8a9d88c2b
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-06-05 10:10:35 +0200

    added protobuf for ownerOf

--- protobufs/gen/go/nft/service.pb.gw.go
+++ protobufs/gen/go/nft/service.pb.gw.go
@@ -103,6 +103,33 @@ func request_NFTService_TokenTransfer_0(ctx context.Context, marshaler runtime.M
 
 }
 
+func request_NFTService_OwnerOf_0(ctx context.Context, marshaler runtime.Marshaler, client NFTServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq OwnerOfRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["token_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "token_id")
+	}
+
+	protoReq.TokenId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "token_id", err)
+	}
+
+	msg, err := client.OwnerOf(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterNFTServiceHandlerFromEndpoint is same as RegisterNFTServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterNFTServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -228,6 +255,35 @@ func RegisterNFTServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_NFTService_OwnerOf_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_NFTService_OwnerOf_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_NFTService_OwnerOf_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -237,6 +293,8 @@ var (
 	pattern_NFTService_MintInvoiceUnpaidNFT_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"token", "mint", "invoice", "unpaid", "identifier"}, ""))
 
 	pattern_NFTService_TokenTransfer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"token", "transfer", "token_id"}, ""))
+
+	pattern_NFTService_OwnerOf_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"token", "owner", "token_id"}, ""))
 )
 
 var (
@@ -245,4 +303,6 @@ var (
 	forward_NFTService_MintInvoiceUnpaidNFT_0 = runtime.ForwardResponseMessage
 
 	forward_NFTService_TokenTransfer_0 = runtime.ForwardResponseMessage
+
+	forward_NFTService_OwnerOf_0 = runtime.ForwardResponseMessage
 )
