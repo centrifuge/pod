45dba4e1a88e2f037556ae783ea70c5a5580b1ad
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-07-17 18:29:12 +0200

    remove unneeded scripts

--- build/scripts/push_to_swagger.sh
+++ build/scripts/push_to_swagger.sh
@@ -2,11 +2,11 @@
 
 # push the swagger api json to swagger hub
 echo "pushing swagger.json to SwaggerHub"
-VERSION=`jq -c '.info.version' protobufs/gen/swagger.json -r`
+VERSION=`jq -c '.info.version' httpapi/swagger.json -r`
 
 curl -i -X POST \
   https://api.swaggerhub.com/apis/centrifuge.io/cent-node?version=$VERSION \
   -H "Authorization: $SWAGGER_API_KEY" \
-  -H "Content-Type: application/json" -d @./protobufs/gen/swagger.json
+  -H "Content-Type: application/json" -d @./httpapi/swagger.json
 
 exit $?
