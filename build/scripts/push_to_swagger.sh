50093cfa2352cf1d5fd367c311e4ae35f01683dd
vedhavyas <vedhavyas.singareddi@gmail.com>
2019-07-18 11:29:03 +0200

    fix diff check

--- build/scripts/push_to_swagger.sh
+++ build/scripts/push_to_swagger.sh
@@ -5,7 +5,7 @@ echo "pushing swagger.json to SwaggerHub"
 VERSION=`jq -c '.info.version' httpapi/swagger.json -r`
 
 curl -i -X POST \
-  https://api.swaggerhub.com/apis/centrifuge.io/cent-node?version=$VERSION \
+  https://api.swaggerhub.com/apis/centrifuge.io/cent-node?version=${VERSION} \
   -H "Authorization: $SWAGGER_API_KEY" \
   -H "Content-Type: application/json" -d @./httpapi/swagger.json
 
