e11e529d7560d0d8ac271136cf0cca7c628f401d
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-07-22 15:21:59 +0200

    babyjub lib

--- crypto/jubjub/jubjub.go
+++ crypto/jubjub/jubjub.go
@@ -2,15 +2,19 @@ package jubjub
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
-	"github.com/pkg/errors"
+	"github.com/centrifuge/go-centrifuge/utils"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/iden3/go-iden3-crypto/babyjub"
+	"math/big"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-// GenerateSigningKeyPair generates a EDDSA JubJub key pair
-func GenerateSigningKeyPair() (publicKey, privateKey []byte, err error) {
+// GenerateSigningKeyPair generates a EDDSA BabyJub key pair
+func GeneratePythonSigningKeyPair() (publicKey, privateKey []byte, err error) {
 	// Resolve python binary location
 	pedBin := os.Getenv("CENT_PED_BIN")
 	if pedBin == "" {
@@ -42,8 +46,46 @@ func GenerateSigningKeyPair() (publicKey, privateKey []byte, err error) {
 	return
 }
 
-func Sign(privateKey, message []byte) (signature []byte, err error) {
-	// Resolve python binary location
+// GenerateSigningKeyPair generates a EDDSA BabyJub key pair
+func GenerateSigningKeyPair() (publicKey, privateKey []byte, err error) {
+	rpk := babyjub.NewRandPrivKey()
+	pkComp := rpk.Public().Compress()
+	publicKey = pkComp[:]
+	privateKey = rpk.Scalar().BigInt().Bytes()
+
+	//// Resolve python binary location
+	//pedBin := os.Getenv("CENT_PED_BIN")
+	//if pedBin == "" {
+	//	pedBin = fmt.Sprintf("%s/zksnarks/zokrates-pycrypto/cli.py", os.Getenv("HOME"))
+	//}
+	//
+	//args := []string{pedBin, "keygen"}
+	//o, err := exec.Command("python3", args...).CombinedOutput()
+	//if err != nil {
+	//	return nil, nil, err
+	//}
+	//
+	//keys := strings.Split(string(o), " ")
+	//if len(keys) < 2 {
+	//	return nil, nil, errors.New("Wrong key format generated")
+	//}
+	//
+	//cleanPKStr := strings.Replace(keys[1], "\n", "", -1)
+	//
+	//privateKey, err = hex.DecodeString(ensureEvenHexLength(keys[0]))
+	//if err !=  nil {
+	//	return nil,nil, err
+	//}
+	//publicKey, err = hex.DecodeString(ensureEvenHexLength(cleanPKStr))
+	//if err !=  nil {
+	//	return nil,nil, err
+	//}
+
+	return
+}
+
+func SignPython(privateKey, message []byte) (signature []byte, err error) {
+	//Resolve python binary location
 	pedBin := os.Getenv("CENT_PED_BIN")
 	if pedBin == "" {
 		pedBin = fmt.Sprintf("%s/zksnarks/zokrates-pycrypto/cli.py", os.Getenv("HOME"))
@@ -72,6 +114,53 @@ func Sign(privateKey, message []byte) (signature []byte, err error) {
 	}
 
 	signature = append(sig1, sig2...)
+	return
+
+}
+
+func Sign(privateKey, message []byte) (signature []byte, err error) {
+	b32, err := utils.SliceToByte32(privateKey)
+	if err != nil {
+		return nil, err
+	}
+	pk := babyjub.PrivateKey(b32)
+	mBI := &big.Int{}
+	mBI = mBI.SetBytes(message)
+	s := pk.SignMimc7(mBI)
+	fmt.Println("R8X_BabyJub", hexutil.Encode(s.R8.X.Bytes()))
+	fmt.Println("R8Y_BabyJub", hexutil.Encode(s.R8.Y.Bytes()))
+	sComp := s.Compress()
+	signature = sComp[:]
+
+	// Resolve python binary location
+	//pedBin := os.Getenv("CENT_PED_BIN")
+	//if pedBin == "" {
+	//	pedBin = fmt.Sprintf("%s/zksnarks/zokrates-pycrypto/cli.py", os.Getenv("HOME"))
+	//}
+	//
+	//args := []string{pedBin, "sig-gen", hex.EncodeToString(privateKey), hex.EncodeToString(message)}
+	//o, err := exec.Command("python3", args...).CombinedOutput()
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//sig := strings.Split(string(o), " ")
+	//if len(sig) < 2 {
+	//	return nil, errors.New("Wrong signature format generated")
+	//}
+	//
+	//cleanSig2 := strings.Replace(sig[1], "\n", "", -1)
+	//sig1, err := hex.DecodeString(ensureEvenHexLength(sig[0]))
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//sig2, err := hex.DecodeString(ensureEvenHexLength(cleanSig2))
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//signature = append(sig1, sig2...)
 	return
 
 }
--- crypto/jubjub/jubjub_test.go
+++ crypto/jubjub/jubjub_test.go
@@ -1,6 +1,10 @@
 package jubjub
 
 import (
+	"fmt"
+	"github.com/centrifuge/go-centrifuge/utils"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/iden3/go-iden3-crypto/babyjub"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -27,13 +31,20 @@ func TestSign(t *testing.T) {
 func TestVerify(t *testing.T) {
 	pk, sk, err := GenerateSigningKeyPair()
 	assert.NoError(t, err)
+	b32, err := utils.SliceToByte32(sk)
+	assert.NoError(t, err)
+	bbpk := babyjub.PrivateKey(b32)
 	msg := []byte("signIt")
 
-	sig, err := Sign(sk, msg)
+	sig, err := SignPython(bbpk[:], msg)
 	assert.NoError(t, err)
-
+	fmt.Println("SignPyCrypto", hexutil.Encode(sig))
+	sig, err = Sign(bbpk[:], msg)
+	assert.NoError(t, err)
+	fmt.Println("LenBabyJub", len(sig))
+	fmt.Println("SignBabyJub", hexutil.Encode(sig))
 	assert.True(t, Verify(pk, msg, sig))
 
-	wrongMsg := []byte("wrong")
-	assert.False(t, Verify(pk, wrongMsg, sig))
+	//wrongMsg := []byte("wrong")
+	//assert.False(t, Verify(pk, wrongMsg, sig))
 }
