bd5ae990a4fbccaf2b6b9ff47c3bda6e102cff24
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-11-20 19:38:28 +0100

    comment back testutil

--- testingutils/setup.go
+++ testingutils/setup.go
@@ -193,8 +193,8 @@ func SetupSmartContractAddresses(cfg config.Configuration, sca *config.SmartCont
 func BuildIntegrationTestingContext() map[string]interface{} {
 	projDir := GetProjectDir()
 	StartPOAGeth()
-	//RunSmartContractMigrations()
-	//RunDAppSmartContractMigrations()
+	RunSmartContractMigrations()
+	RunDAppSmartContractMigrations()
 	addresses := GetSmartContractAddresses()
 	cfg := LoadTestConfig()
 	cfg.Set("keys.p2p.publicKey", fmt.Sprintf("%s/build/resources/p2pKey.pub.pem", projDir))
