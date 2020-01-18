4e2e4c8d5638cbcd05b3183a8d2c30cb6e80c6e1
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2020-01-19 00:44:21 +0100

    fix path

--- build/scripts/docker/run.sh
+++ build/scripts/docker/run.sh
@@ -76,8 +76,8 @@ case "$mode" in
     docker-compose -f $local_dir/docker-compose-cc.yml up > /tmp/cc-0.log 2>&1 &
   ;;
   bridge)
-    BRIDGE_CONFIGDIR=$PWD/build/scripts/test-dependencies/test-bridge/config/
-    BRIDGE_KEYSDIR=$PWD/build/scripts/test-dependencies/test-bridge/keys/
+    BRIDGE_CONFIGDIR=$PWD/build/scripts/test-dependencies/test-xbridge/config/
+    BRIDGE_KEYSDIR=$PWD/build/scripts/test-dependencies/test-xbridge/keys/
     BRIDGE_CONFIGDIR=$BRIDGE_CONFIGDIR BRIDGE_KEYSDIR=$BRIDGE_KEYSDIR docker-compose -f $local_dir/docker-compose-bridge.yml up > /tmp/bridge-0.log 2>&1 &
   ;;
   *) usage
--- build/scripts/test-dependencies/test-xbridge/add_balance.sh
+++ build/scripts/test-dependencies/test-xbridge/add_balance.sh
@@ -5,6 +5,6 @@ export MIGRATE_ADDRESS=${MIGRATE_ADDRESS:-'0x89b0a86583c4444acfd71b463e0d3c55ae1
 export MIGRATE_PASSWORD=${MIGRATE_PASSWORD:-''}
 export DEV_BRIDGE_ADDRESS=${DEV_BRIDGE_ADDRESS:-'0x56e77cD98C241b0Fb70Bc83A8eF41D94a30C6f1e'}
 
-echo "Adding balance to Bridge Address [${DEV_BRIDGE_ADDRESS}]"
+echo "Adding some balance to Bridge Address [${DEV_BRIDGE_ADDRESS}]"
 docker run -it --net=host --entrypoint "/geth" centrifugeio/cent-geth:v0.1.1 attach http://localhost:9545 --exec "personal.unlockAccount('${MIGRATE_ADDRESS}', '${MIGRATE_PASSWORD}', 500)"
 docker run -it --net=host --entrypoint "/geth" centrifugeio/cent-geth:v0.1.1 attach http://localhost:9545 --exec "eth.sendTransaction({from:'${MIGRATE_ADDRESS}', to:'${DEV_BRIDGE_ADDRESS}', value: web3.toWei(10, 'ether'), gas:21000});"
