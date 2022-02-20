06fa31859e012062684490fbb80c4e937571f348
Lorenzo Manacorda <lorenzo@mailbox.org>
2022-02-20 16:57:38 +0100

    nix: further work

--- flake.nix
+++ flake.nix
@@ -1,49 +1,70 @@
 {
   description = "go-centrifuge";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    gitignore = {
+      url = github:hercules-ci/gitignore.nix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, gitignore }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
       name = "go-centrifuge";
       version = "2.0";
+
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      gitignoreFilter = gitignore.lib.gitignoreFilter;
+
+      srcFilter = src:
+        let
+          srcIgnored = gitignoreFilter src;
+        in
+        path: type:
+          let
+            p = builtins.baseNameOf path;
+          in
+          srcIgnored path type
+          || (p == "flake.nix");
     in
     {
-
       packages.${system} = {
-        go-centrifuge = pkgs.buildGoModule rec {
+        go-centrifuge = pkgs.buildGoModule {
           pname = name;
           inherit version;
 
-          src = ./.;
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = srcFilter ./.;
+            name = "${name}-source";
+          };
 
           vendorSha256 = "sha256-Yr1lxkeW9rvOR+tLn9YBE682hgatsLGRqsalhcz+r9Y=";
         };
 
-        dockerContainer =
-          pkgs.dockerTools.buildImage {
-            name = "centrifugeio/${name}";
-            tag = version;
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "centrifugeio/${name}";
+          tag = version;
 
-            contents = self.defaultPackage.${system};
+          contents = [ pkgs.busybox self.defaultPackage.${system} ];
 
-            config = {
-              Env = [
-                "PATH=/bin:$PATH"
-              ];
-              ExposedPorts = {
-                "30333/tcp" = { };
-                "9933/tcp" = { };
-                "9944/tcp" = { };
-              };
-              Volumes = {
-                "/data" = { };
-              };
-              Entrypoint = [ "centrifuge" ];
+          config = {
+            # TODO what ports do we actually need?
+            ExposedPorts = {
+              # api
+              "8082/tcp" = { };
+              # p2p
+              "38202/tcp" = { };
             };
+            Volumes = {
+              "/data" = { };
+            };
+            Entrypoint = [ "centrifuge" ];
           };
+        };
       };
 
       defaultPackage.${system} = self.packages.${system}.go-centrifuge;
@@ -54,7 +75,5 @@
       };
 
       defaultApp.${system} = self.apps.${system}.centrifuge;
-
-
     };
 }
